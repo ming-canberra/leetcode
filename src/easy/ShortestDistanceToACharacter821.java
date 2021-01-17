d2a346d2f804c537c402c5ed75f82cccf018a6a7
Ming Chen <ming.chen@vixverify.com>
2021-01-18 00:42:25 +1100

    new file

---
+++ src/easy/ShortestDistanceToACharacter821.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class ShortestDistanceToACharacter821 {
+    public static void main(String[] args) {
+        ShortestDistanceToACharacter821 thisClass = new ShortestDistanceToACharacter821();
+        System.out.println(thisClass.shortestToChar("abcde",'c'));
+    }
+    public int[] shortestToChar(String S, char C) {
+
+        return new int[]{1,2,3};
+    }
+}
