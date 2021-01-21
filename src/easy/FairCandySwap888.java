560f33fe980b5d4e843eed58cde407d2afcc84bc
Ming Chen <ming.chen@edentiti.com>
2021-01-22 00:18:32 +1100

    fair candy swap

---
+++ src/easy/FairCandySwap888.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class FairCandySwap888 {
+    public static void main(String[] args) {
+        FairCandySwap888 thisClass = new FairCandySwap888();
+
+        thisClass.fairCandySwap(null, null);
+    }
+    //do binary search
+    public int[] fairCandySwap(int[] a, int[] b) {
+
+        return a;
+    }
+}
