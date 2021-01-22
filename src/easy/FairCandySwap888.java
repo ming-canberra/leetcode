9730185d6fc3e19102d6d1054da33dd9726b15c0
Ming Chen <ming.chen@edentiti.com>
2021-01-23 02:22:26 +1100

    did 1 today

--- src/easy/FairCandySwap888.java
+++ src/easy/FairCandySwap888.java
@@ -1,5 +1,7 @@
 package easy;
 
+import java.util.HashSet;
+
 public class FairCandySwap888 {
     public static void main(String[] args) {
         FairCandySwap888 thisClass = new FairCandySwap888();
@@ -8,7 +10,39 @@ public class FairCandySwap888 {
     }
     //do binary search
     public int[] fairCandySwap(int[] a, int[] b) {
+        int sumA = 0;
+        int sumB = 0;
+        HashSet<Integer> setA = new HashSet<Integer>();
+        HashSet<Integer> setB = new HashSet<Integer>();
 
-        return a;
+        for (int i = 0; i < a.length;i++){
+            sumA += a[i];
+            setA.add(a[i]);
+        }
+        for (int i = 0; i < b.length;i++){
+            sumB += b[i];
+            setB.add(b[i]);
+        }
+        int diff = 0;
+        if (sumA > sumB){
+            diff = (sumA -sumB)/2;
+            for (int i = 0; i < b.length;i++)
+            {
+                if (setA.contains(diff + b[i])){
+                    return new int[]{diff + b[i], b[i]};
+                }
+            }
+        }
+        else{
+            diff = (sumB -sumA)/2;
+            for (int i = 0; i < a.length;i++)
+            {
+                if (setB.contains(diff + a[i])){
+                    return new int[]{a[i], diff + a[i]};
+                }
+            }
+        }
+
+        return null;
     }
 }
