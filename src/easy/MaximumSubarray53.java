108326abc02aedf5a251e26f0c21b8f7bcd693aa
Ming Chen <ming.chen@edentiti.com>
2021-01-23 18:12:56 +1100

    did 1 today, an old one that I didnot come up with a solution

--- src/easy/MaximumSubarray53.java
+++ src/easy/MaximumSubarray53.java
@@ -4,20 +4,32 @@ public class MaximumSubarray53 {
     public static void main(String[] args) {
         MaximumSubarray53 thisClass = new MaximumSubarray53();
 
-        System.out.println(thisClass.maxSubArray(new int[]{1,3,4,5,3}));
+        System.out.println(thisClass.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
     }
     public int maxSubArray(int[] nums) {
 
-        int[]sum = new int[nums.length];
-
+        int ans = Integer.MIN_VALUE;
+        int current = Integer.MIN_VALUE;
         for (int i =0;i < nums.length;i++){
-            
-
-
+            if (nums[i]>0){
+                if (current <=0){
+                    current = nums[i];
+                }
+                else{
+                    current +=nums[i];
+                }
+            }
+            else{
+                if (current <=0){
+                    current = nums[i];
+                }
+                else{
+                    current +=nums[i];
+                }
+            }
+            ans = Math.max(ans, current);
         }
 
-
-        return 1254;
+        return ans;
     }
-
 }
