45b24bfab51567eda2da353de7a9183ee9e1bf31
Ming Chen <ming.chen@edentiti.com>
2021-01-29 02:01:09 +1100

    did one today, a bit slow. learned that using linkedlist as queue instead of priority queue

--- src/medium/RevealCardsInIncreasingOrder950.java
+++ src/medium/RevealCardsInIncreasingOrder950.java
@@ -1,12 +1,31 @@
 package medium;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RevealCardsInIncreasingOrder950 {
     public static void main(String[] args) {
         RevealCardsInIncreasingOrder950 thisClass = new RevealCardsInIncreasingOrder950();
-        System.out.println(" " + thisClass.deckRevealedIncreasing(new int[]{1,2,3,4}));
+        System.out.println(" " + thisClass.deckRevealedIncreasing(new int[]{17,13,11,2,3,5,7}));
     }
     public int[] deckRevealedIncreasing(int[] deck) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        Arrays.sort(deck);
+        for (int i = deck.length - 1; i>=0; i -- ){
+            if (!q.isEmpty()){
+                Integer tmp = q.poll();
+                q.add(tmp);
+            }
+            q.add(deck[i]);
+        }
 
-        return null;
+        int[] ans = new int[deck.length];
+
+        for (int i = ans.length -1; i>=0;i--){
+            ans[i] = q.poll();
+        }
+
+        return ans;
     }
 }
