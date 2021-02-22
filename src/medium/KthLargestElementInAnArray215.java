8f398c7b12f544c92aa420d1a1b66965248294c2
Ming Chen <ming.chen@edentiti.com>
2021-02-22 23:56:58 +1100

    quick sort with pivot

--- src/medium/KthLargestElementInAnArray215.java
+++ src/medium/KthLargestElementInAnArray215.java
@@ -7,10 +7,42 @@ import java.util.Queue;
 public class KthLargestElementInAnArray215 {
     public static void main(String[] args) {
         KthLargestElementInAnArray215 thisClass = new KthLargestElementInAnArray215();
-        System.out.println(" " + thisClass.findKthLargest(new int[]{17,13,11,2,3,5,7}, 3));
+        System.out.println(" " + thisClass.findKthLargest(new int[]{3,2,1,5,6,4}, 2));
     }
-    public int findKthLargest(int[] nums, int k) {
+    public int findKthLargest2(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length -k];
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        rearragePivoting(nums, 0, nums.length -1);
+        return nums[nums.length -k];
+    }
+
+    private void rearragePivoting(int[] nums, int l, int r){
+        if (l <r){
+            int rValue = nums[r];
+            int startIndex = l;
+            for (int i = l; i <r; i++){
+                if(nums[i] <= rValue){
+                    swap(nums, startIndex++, i);
+                }
+            }
+            swap(nums, startIndex, r);
+
+
+            int pivotIndex = startIndex;
+
+            rearragePivoting(nums, l, pivotIndex -1);
+
+            rearragePivoting(nums,pivotIndex + 1, r);
+
+        }
+    }
+
+    private void swap (int[] nums, int a, int b){
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
     }
 }
