26635933423e75ba5366f19410d1caa258217644
Ming Chen <ming.chen@edentiti.com>
2021-02-28 21:55:34 +1100

    tried a few dozen times before I got it right.

---
+++ src/medium/FindMinimumInRotatedSortedArray153.java
@@ -0,0 +1,11 @@
+package medium;
+
+public class FindMinimumInRotatedSortedArray153 {
+    public static void main(String[] args) {
+        FindMinimumInRotatedSortedArray153 thisClass = new FindMinimumInRotatedSortedArray153();
+        System.out.println (thisClass.findMin(new int[]{2,0,2,1,1,0}));
+    }
+    public int findMin(int[] nums) {
+        return 231;
+    }
+}
