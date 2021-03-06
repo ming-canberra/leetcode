091bd4de1b43167137ff2a0f3cf270b3621a7970
Ming Chen <ming.chen@edentiti.com>
2021-03-07 00:23:16 +1100

    This works

---
+++ src/easy/PalindromeLinkedList234.java
@@ -0,0 +1,17 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PalindromeLinkedList234 {
+    public static void main(String[] args) {
+        PalindromeLinkedList234 thisClass = new PalindromeLinkedList234();
+        boolean df = thisClass.isPalindrome(null);
+        System.out.println(df);
+    }
+    public boolean isPalindrome(ListNode head) {
+        
+        return true;
+    }
+}
