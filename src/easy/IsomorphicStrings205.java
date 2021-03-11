e3e281c078794ae897aadc48967d0240ae36228f
Ming <ming.edentiti@gmail.com>
2021-03-11 12:52:27 +1100

    this is good

---
+++ src/easy/IsomorphicStrings205.java
@@ -0,0 +1,14 @@
+package easy;
+
+import java.util.HashMap;
+
+public class IsomorphicStrings205 {
+    public static void main(String[] args) {
+        IsomorphicStrings205 thisClass = new IsomorphicStrings205();
+
+        System.out.println(thisClass.isIsomorphic("abccccdd", "f"));
+    }
+    public boolean isIsomorphic(String s, String t) {
+        return false;
+    }
+}
--- src/easy/LongestPalindrome409.java
+++ src/easy/LongestPalindrome409.java
@@ -8,7 +8,8 @@ public class LongestPalindrome409 {
 
         System.out.println(thisClass.longestPalindrome("abccccdd"));
     }
-    public int longestPalindrome(String s) {
+
+    public int longestPalindrome2(String s) {
         HashMap<Character, Integer> charFrequency = new HashMap<Character, Integer>();
         for (int i =0; i < s.length(); i++){
             if (charFrequency.containsKey(s.charAt(i))){
@@ -34,5 +35,24 @@ public class LongestPalindrome409 {
         if (countSingleOnce)
             toReturn = toReturn +1;
         return toReturn;
+    }
+
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> theMap = new HashMap<Character, Integer>();
+        int result = 0;
+        for (int i =0; i < s.length(); i++){
+            theMap.put(s.charAt(i), theMap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        boolean hasOne = false;
+        for (char key : theMap.keySet()){
+            result += theMap.get(key) / 2 * 2;
+            if (!hasOne && theMap.get(key) % 2 ==1){
+                hasOne = true;
+            }
+        }
+        if(hasOne){
+            result++;
+        }
+        return result;
     }
 }
