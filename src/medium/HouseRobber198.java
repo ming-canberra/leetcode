667fa95d4ac9a4b144edf30bf8d71e613ec08ebb
Ming <ming.edentiti@gmail.com>
2021-03-16 00:06:37 +1100

    backbone

---
+++ src/medium/HouseRobber198.java
@@ -0,0 +1,14 @@
+package medium;
+
+public class HouseRobber198 {
+    public static void main(String[] args) {
+        HouseRobber198 thisClass = new HouseRobber198();
+
+        int res = thisClass.rob(new int[]{5,4,0,3,1,6,2});
+        System.out.println(res);
+    }
+    public int rob(int[] nums) {
+
+        return 142;
+    }
+}
