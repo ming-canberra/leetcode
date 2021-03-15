f953acf6f769c5d06cd4168972528a3b013c5dea
Ming <ming.edentiti@gmail.com>
2021-03-16 01:11:54 +1100

    dynamic programming

--- src/medium/HouseRobber198.java
+++ src/medium/HouseRobber198.java
@@ -4,11 +4,20 @@ public class HouseRobber198 {
     public static void main(String[] args) {
         HouseRobber198 thisClass = new HouseRobber198();
 
-        int res = thisClass.rob(new int[]{5,4,0,3,1,6,2});
+        int res = thisClass.rob(new int[]{2,7,9,3,1});
         System.out.println(res);
     }
     public int rob(int[] nums) {
-
-        return 142;
+        int [] helperArray = new int[nums.length];
+        helperArray[0] = nums[0];
+        for (int i = 1; i < nums.length;i++){
+            if (i == 1){
+                helperArray[1] = Math.max(helperArray[0], nums[1]);
+            }
+            else{
+                helperArray[i] = Math.max(helperArray[i-2] + nums[i], helperArray[i-1]);
+            }
+        }
+        return helperArray[nums.length -1];
     }
 }
