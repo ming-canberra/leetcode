ec2dc984829a3c0c5bc62fd348a82d30a75081ca
Ming Chen <ming.chen@edentiti.com>
2021-03-16 23:27:58 +1100

    code framework

---
+++ src/medium/MinimumPathSum64.java
@@ -0,0 +1,14 @@
+package medium;
+
+public class MinimumPathSum64 {
+    public static void main(String[] args) {
+        MinimumPathSum64 thisClass = new MinimumPathSum64();
+
+        int res = thisClass.minPathSum(null);
+        System.out.println(res);
+    }
+    public int minPathSum(int[][] grid) {
+
+        return 123;
+    }
+}
