40c534bf73f482d77a7039c0362ff2d86e8dccd1
Ming <ming.edentiti@gmail.com>
2021-03-17 13:21:07 +1100

    so happy, easy dp one

---
+++ src/medium/UniquePaths62.java
@@ -0,0 +1,26 @@
+package medium;
+
+public class UniquePaths62 {
+    public static void main(String[] args) {
+        UniquePaths62 thisClass = new UniquePaths62();
+
+        int res = thisClass.uniquePaths(4,5);
+        System.out.println(res);
+    }
+    public int uniquePaths(int m, int n) {
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m;i++){
+            for (int j = 0; j < n; j++){
+                if (i == 0 || j ==0){
+                    dp[i][j] =1;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
