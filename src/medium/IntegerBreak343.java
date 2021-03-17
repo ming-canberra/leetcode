395164660e1058a7dbca0201dfa4f456b13324b4
Ming <ming.edentiti@gmail.com>
2021-03-18 01:18:44 +1100

    get ready for this

---
+++ src/medium/IntegerBreak343.java
@@ -0,0 +1,13 @@
+package medium;
+
+public class IntegerBreak343 {
+    public static void main(String[] args) {
+        IntegerBreak343 thisClass = new IntegerBreak343();
+
+        int res = thisClass.integerBreak(2345);
+        System.out.println(res);
+    }
+    public int integerBreak(int n) {
+        return 123;
+    }
+}
