f46e9ef99518ea0d0695c13942175bafe44d07e2
Ming Chen <ming.chen@edentiti.com>
2021-03-19 23:34:27 +1100

    scratch code

---
+++ src/medium/WiggleSubsequence376.java
@@ -0,0 +1,15 @@
+package medium;
+
+import java.util.Arrays;
+
+public class WiggleSubsequence376 {
+    public static void main(String[] args) {
+        WiggleSubsequence376 thisClass = new WiggleSubsequence376();
+        int res = thisClass.wiggleMaxLength();
+        System.out.println(res);
+    }
+
+    public int wiggleMaxLength(int[] nums) {
+        return 234;
+    }
+}
