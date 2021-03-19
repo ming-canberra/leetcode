f1b590c944d54d62ac2184ff8ba0a0e08c273fd8
Ming <ming.edentiti@gmail.com>
2021-03-19 21:29:56 +1100

    this is too hard, try another day maybe

---
+++ src/medium/LongestIncreasingSubsequence300.java
@@ -0,0 +1,19 @@
+package medium;
+
+import java.util.HashMap;
+
+public class LongestIncreasingSubsequence300 {
+    public static void main(String[] args) {
+        LongestIncreasingSubsequence300 thisClass = new LongestIncreasingSubsequence300();
+
+        int res = thisClass.lengthOfLIS(new int[]{10,9,2,5,3,7,101,18});
+        System.out.println(res);
+    }
+
+    public int lengthOfLIS(int[] nums) {
+
+
+
+        return 234;
+    }
+}
