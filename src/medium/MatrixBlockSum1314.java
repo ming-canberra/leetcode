92ca5e4a2e02c3853322fae2ac59afefa996508b
Ming <ming.edentiti@gmail.com>
2021-03-23 02:30:17 +1100

    finally, but this took so long

---
+++ src/medium/MatrixBlockSum1314.java
@@ -0,0 +1,17 @@
+package medium;
+
+import java.util.Arrays;
+
+public class MatrixBlockSum1314 {
+    public static void main(String[] args) {
+        MatrixBlockSum1314 thisClass = new MatrixBlockSum1314();
+        thisClass.matrixBlockSum(null,123);
+    }
+
+    public int[][] matrixBlockSum(int[][] mat, int K) {
+
+        Arrays.sort(mat, (a, b)->a[0]-b[0]);
+
+        return null;
+    }
+}
---
+++ src/medium/RangeSumQuery2DImmutable304.java
@@ -0,0 +1,30 @@
+package medium;
+
+import java.util.Arrays;
+
+public class RangeSumQuery2DImmutable304 {
+    public static void main(String[] args) {
+        RangeSumQuery2DImmutable304 thisClass = new RangeSumQuery2DImmutable304(null);
+    }
+
+
+    private int[][] dp;
+
+    public RangeSumQuery2DImmutable304(int[][] matrix) {
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+
+        for (int i = 1; i < dp.length;i++){
+            for (int j = 1 ; j < dp[0].length;j++){
+                dp[i][j] =dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+
+
+
+
+        return dp[row2 + 1][col2 + 1] + dp[row1 ][col1 ] - dp[row1 ][col2 +1 ] - dp[row2 + 1][col1];
+    }
+}
