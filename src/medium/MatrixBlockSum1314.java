ad1d4ac07bc62d4415c4906d1ba1df3ab5169c89
Ming <ming.edentiti@gmail.com>
2021-03-24 00:26:32 +1100

    use Math.max(0, i - K) could save some coding

--- src/medium/MatrixBlockSum1314.java
+++ src/medium/MatrixBlockSum1314.java
@@ -1,17 +1,54 @@
 package medium;
 
 import java.util.Arrays;
+//[[67,64,78],[99,98,38],[82,46,46],[6,52,55],[55,99,45]]
 
 public class MatrixBlockSum1314 {
     public static void main(String[] args) {
         MatrixBlockSum1314 thisClass = new MatrixBlockSum1314();
-        thisClass.matrixBlockSum(null,123);
+        thisClass.matrixBlockSum(new int[][]{new int[]{67,64,78}, new int[]{99,98,38}, new int[]{82,46,46},
+        new int[]{6,52,55}, new int[]{55,99,45}},3);
     }
 
-    public int[][] matrixBlockSum(int[][] mat, int K) {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
 
-        Arrays.sort(mat, (a, b)->a[0]-b[0]);
+        int m = mat.length;
+        int n = mat[0].length;
 
-        return null;
+        //each sum[i][j] stores the sum of all values BEFORE the current position of the mapped mat position,
+        //also add k length padding surrounding the original 'mat'
+        int sum[][] = new int [m + k + k + 1][n + k + k + 1];
+
+        for (int i = 0 ; i < sum.length; i++){
+            for (int j = 0 ; j < sum[0].length; j++)
+            {
+                if (i < k + 1){
+                    sum[i][j] = 0;
+                }
+                else if (j < k + 1){
+                    sum[i][j] = 0;
+                }
+                else if( i >= m + k + 1){
+                    sum[i][j] = sum[m + k][j];
+                }
+                else if( j >= n + k + 1){
+                    sum[i][j] = sum[i][n + k];
+                }
+                else{
+                    sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + mat[i - k - 1][j - k - 1];
+                }
+            }
+        }
+
+        int toReturn[][] = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                toReturn[i][j] = sum[i + k + k + 1][j + k + k + 1]
+                        - sum[i + k + k + 1][j] - sum[i][j + k + k + 1] + sum[i][j];
+            }
+        }
+
+
+        return toReturn;
     }
 }
