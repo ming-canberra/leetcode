6f9788d35422dd7383ccaa8b688766002d350f83
Ming <ming.edentiti@gmail.com>
2021-03-24 23:41:23 +1100

    scratch code

---
+++ src/medium/CountingBits338.java
@@ -0,0 +1,13 @@
+package medium;
+
+public class CountingBits338 {
+    public static void main(String[] args) {
+        CountingBits338 thisClass = new CountingBits338();
+        thisClass.countBits(234);
+    }
+
+    public int[] countBits(int num) {
+
+        return null;
+    }
+}
