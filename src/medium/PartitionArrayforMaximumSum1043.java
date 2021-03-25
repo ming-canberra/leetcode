75ccf1f3a7aafc27bf1c224f50ba4411b0ad78f8
Ming <ming.edentiti@gmail.com>
2021-03-25 17:35:51 +1100

    backbone code

---
+++ src/medium/PartitionArrayforMaximumSum1043.java
@@ -0,0 +1,15 @@
+package medium;
+
+public class PartitionArrayforMaximumSum1043 {
+    public static void main(String[] args) {
+        PartitionArrayforMaximumSum1043 thisClass = new PartitionArrayforMaximumSum1043();
+
+        int res = thisClass.maxSumAfterPartitioning(new int[]{5,4,0,3,1,6,2}, 3);
+        System.out.println(res);
+    }
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+
+
+        return 123;
+    }
+}
