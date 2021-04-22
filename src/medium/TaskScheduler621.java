bb94ecd76f7fa2bd57aec9519f345414ccaa0123
Ming <ming.edentiti@gmail.com>
2021-04-22 23:46:16 +1000

    My first knapsack needs improvements.

---
+++ src/medium/TaskScheduler621.java
@@ -0,0 +1,12 @@
+package medium;
+
+public class TaskScheduler621 {
+    public static void main(String[] args) {
+        TaskScheduler621 thisClass = new TaskScheduler621();
+
+        System.out.println (thisClass.leastInterval(null, 3));
+    }
+    public int leastInterval(char[] tasks, int n) {
+        return 134;
+    }
+}
