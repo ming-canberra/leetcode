815c26944823a8d67a7d1ad9376f1ca3ed33b234
Ming <ming.edentiti@gmail.com>
2021-04-23 22:50:24 +1000

    just wrote the clean code for binary search again.

--- src/easy/BinarySearch704.java
+++ src/easy/BinarySearch704.java
@@ -11,6 +11,30 @@ public class BinarySearch704 {
 
         System.out.println(thisClass.isHappy(13));
     }
+
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r){
+            int m = l + (r - l) / 2;
+            if (nums[m] == target){
+                return m;
+            }
+            else if (nums[m] > target){
+                r = m - 1;
+            }
+            else{
+                l = m + 1;
+            }
+        }
+
+        return -1;
+    }
+
+
+
+
     public boolean isHappy(int n) {
         HashSet<Integer> set = new HashSet<Integer>();
         int current = n;
---
+++ src/medium/FindAllAnagramsInAString.java
@@ -0,0 +1,20 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllAnagramsInAString {
+    public static void main(String[] args) {
+        FindAllAnagramsInAString thisClass = new FindAllAnagramsInAString();
+
+        thisClass.findAnagrams("null", "");
+    }
+    public List<Integer> findAnagrams(String s, String p) {
+
+
+
+
+        List<Integer> result = new ArrayList<Integer>();
+        return result;
+    }
+}
