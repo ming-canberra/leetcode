03c45852319abae854ce0be367fd4c225e26d335
Ming <ming.edentiti@gmail.com>
2021-04-25 00:29:01 +1000

    more concise version of it

--- src/easy/AddStrings415.java
+++ src/easy/AddStrings415.java
@@ -6,20 +6,20 @@ import java.util.List;
 public class AddStrings415 {
     public static void main(String[] args) {
         AddStrings415 thisClass = new AddStrings415();
-        thisClass.addStrings("408","5");
+        System.out.println(thisClass.addStrings2("408", "5"));
 
-StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         //System.out.println(sb.append(1).append(2).append(3).reverse());
     }
+
     public String addStrings(String num1, String num2) {
 
         String longerString;
         String shorterString;
-        if (num1.length() > num2.length()){
+        if (num1.length() > num2.length()) {
             longerString = num1;
             shorterString = num2;
-        }
-        else{
+        } else {
             longerString = num2;
             shorterString = num1;
         }
@@ -29,77 +29,93 @@ StringBuilder sb = new StringBuilder();
 
         String addedStringCommonLength = "";
         boolean plusOneFromLastRound = false;
-        for (int i = partLongerStringDoAdding.length() -1; i >=0;i --){
-            int firstDigit = new Integer (partLongerStringDoAdding.substring(i, i+1));
-            int secondDigit = new Integer (shorterString.substring(i, i+1));
+        for (int i = partLongerStringDoAdding.length() - 1; i >= 0; i--) {
+            int firstDigit = new Integer(partLongerStringDoAdding.substring(i, i + 1));
+            int secondDigit = new Integer(shorterString.substring(i, i + 1));
             int digitsAdded = 0;
-            if (plusOneFromLastRound){
-                digitsAdded = firstDigit + secondDigit +1;
-            }
-            else{
+            if (plusOneFromLastRound) {
+                digitsAdded = firstDigit + secondDigit + 1;
+            } else {
                 digitsAdded = firstDigit + secondDigit;
             }
-            if (digitsAdded > 9){
+            if (digitsAdded > 9) {
                 plusOneFromLastRound = true;
                 digitsAdded = digitsAdded % 10;
-            }
-            else{
+            } else {
                 plusOneFromLastRound = false;
             }
             addedStringCommonLength = new Integer(digitsAdded) + addedStringCommonLength;
         }
 
-        String toReturn  ="";
-        if (plusOneFromLastRound){
-            if (leadingPartLongerStringLength >0 ){
+        String toReturn = "";
+        if (plusOneFromLastRound) {
+            if (leadingPartLongerStringLength > 0) {
                 //handle leading part
-                if (leadingPartLongerStringLength == 1 ){
-                    toReturn = new Integer (new Integer (longerString.substring(0, 1)) +1).toString()
+                if (leadingPartLongerStringLength == 1) {
+                    toReturn = new Integer(new Integer(longerString.substring(0, 1)) + 1).toString()
                             + addedStringCommonLength;
-                }
-                else{
+                } else {
                     String leadingPartAddedOne = "";
                     String leadingPartLongerString = longerString.substring(0, leadingPartLongerStringLength);
                     boolean carry = true;
-                    for (int i = leadingPartLongerString.length() -1; i >=0;i--){
-                        int currentDigit = new Integer (leadingPartLongerString.substring(i, i+1));
-                        if (currentDigit ==9 && carry){
+                    for (int i = leadingPartLongerString.length() - 1; i >= 0; i--) {
+                        int currentDigit = new Integer(leadingPartLongerString.substring(i, i + 1));
+                        if (currentDigit == 9 && carry) {
                             carry = true;
                             leadingPartAddedOne = "0" + leadingPartAddedOne;
-                        }
-                        else{
-                            if (carry){
-                                leadingPartAddedOne = (currentDigit +1 ) + leadingPartAddedOne;
+                        } else {
+                            if (carry) {
+                                leadingPartAddedOne = (currentDigit + 1) + leadingPartAddedOne;
 
-                            }
-                            else {
+                            } else {
                                 leadingPartAddedOne = currentDigit + leadingPartAddedOne;
                             }
                             carry = false;
                         }
                     }
-                    if (carry){
+                    if (carry) {
                         leadingPartAddedOne = "1" + leadingPartAddedOne;
                     }
                     toReturn = leadingPartAddedOne + addedStringCommonLength;
                 }
-            }
-            else{
+            } else {
                 toReturn = "1" + addedStringCommonLength;
             }
-        }
-        else{
-            if (leadingPartLongerStringLength >0 ){
+        } else {
+            if (leadingPartLongerStringLength > 0) {
                 //handle leading part
-                toReturn =  longerString.substring(0, leadingPartLongerStringLength) + addedStringCommonLength;
-            }
-            else{
+                toReturn = longerString.substring(0, leadingPartLongerStringLength) + addedStringCommonLength;
+            } else {
                 toReturn = addedStringCommonLength;
             }
         }
 
 
-
         return toReturn;
+    }
+
+    public String addStrings2(String num1, String num2) {
+        int length = Math.max(num1.length(), num2.length());
+        int index = 0;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (index < length) {
+            int digit1 = 0;
+            if (num1.length() - index > 0) {
+                digit1 = num1.charAt(num1.length() - index - 1) - '0';
+            }
+            int digit2 = 0;
+            if (num2.length() - index > 0) {
+                digit2 = num2.charAt(num2.length() - index - 1) - '0';
+            }
+            int newDigit = (digit1 + digit2 + carry) % 10;
+            carry = (digit1 + digit2 + carry) / 10;
+            sb.append(Integer.valueOf(newDigit).toString());
+            index++;
+        }
+        if (carry > 0) {
+            sb.append("1");
+        }
+        return sb.reverse().toString();
     }
 }
--- src/medium/PathSumIII.java
+++ src/medium/PathSumIII.java
@@ -1,7 +1,7 @@
 package medium;
 
-import easy.TreeNode;
-
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class PathSumIII {
@@ -9,13 +9,28 @@ public class PathSumIII {
         PathSumIII thisClass = new PathSumIII();
         thisClass.pathSum(null, 1);
     }
-
     private static int _counter = 0;
     public int pathSum(TreeNode root, int targetSum) {
-
-
-
-
+        sum(root, new HashMap<Integer, Integer>(), targetSum);
         return _counter;
+    }
+    private void sum(TreeNode root, HashMap<Integer, Integer> map, int target){
+        if (root != null){
+            if (map.containsKey(root.val)){
+                _counter = _counter + map.get(root.val);
+            }
+
+            HashMap<Integer, Integer> descendantMap = new HashMap<Integer, Integer>();
+
+            for (Object key : map.keySet()){
+                descendantMap.put((Integer)key - root.val, map.get(key));
+            }
+
+            int forChildren = target - root.val;
+            descendantMap.put(forChildren, descendantMap.getOrDefault(forChildren, 0) + 1);
+
+            sum(root.left, descendantMap, target);
+            sum(root.right, descendantMap, target);
+        }
     }
 }
