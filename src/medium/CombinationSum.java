4a9bfe324ae31efad5db4e6e0f0308fa13a83471
Ming <ming.edentiti@gmail.com>
2021-04-26 00:07:16 +1000

    cheated again

--- src/medium/CombinationSum.java
+++ src/medium/CombinationSum.java
@@ -2,6 +2,8 @@ package medium;
 
 import easy.ListNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
@@ -11,6 +13,23 @@ public class CombinationSum {
         System.out.println (thisClass.combinationSum(null, 3));
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(candidates);
+        List<Integer> intList = new ArrayList<Integer>();
+        sum(candidates, target, result, 0, new ArrayList<Integer>(), 0);
+        return result;
+    }
+    private void sum(int[] candidates, int target, List<List<Integer>> result, int prevSum, List<Integer> prevList,
+                     int startingIndex){
+        if (prevSum < target){
+            for (int i = startingIndex; i < candidates.length; i++){
+                prevList.add(candidates[i]);
+                sum(candidates, target, result, prevSum + candidates[i], prevList, i);
+                prevList.remove(prevList.size() - 1);
+            }
+        }
+        else if (prevSum == target){
+            result.add(new ArrayList<Integer>(prevList));
+        }
     }
 }
