f54e2671a9f961c3630f97206a9465845328a975
Ming <ming.edentiti@gmail.com>
2021-04-26 02:04:37 +1000

    scratch code, get ready

---
+++ src/medium/Subsets78.java
@@ -0,0 +1,21 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets78 {
+    public static void main(String[] args) {
+        Subsets78 thisClass = new Subsets78();
+        System.out.println (thisClass.subsets(null));
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+
+        return result;
+    }
+
+    private void generateSub(List<List<Integer>> result){
+
+    }
+}
