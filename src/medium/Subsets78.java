8a859708033439215c5bd0fd4c9008b5ef8dc7e5
Ming <ming.edentiti@gmail.com>
2021-04-26 11:40:28 +1000

    almost nailed it

--- src/medium/Subsets78.java
+++ src/medium/Subsets78.java
@@ -8,14 +8,20 @@ public class Subsets78 {
         Subsets78 thisClass = new Subsets78();
         System.out.println (thisClass.subsets(null));
     }
+
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-
-
+        result.add(new ArrayList<Integer>());
+        generateSub(result, nums, 0, new ArrayList<Integer>());
         return result;
     }
 
-    private void generateSub(List<List<Integer>> result){
-
+    private void generateSub(List<List<Integer>> result, int[] nums, int sIndex, List<Integer> tResult) {
+        for (int i = sIndex; i < nums.length; i++) {
+            tResult.add(nums[i]);
+            result.add(new ArrayList<>(tResult));
+            generateSub(result, nums, i + 1, tResult);
+            tResult.remove(tResult.size() - 1);
+        }
     }
 }
