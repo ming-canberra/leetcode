d26a2bf92010c55f002dcc1f09ba43c300c205fe
Ming <ming.edentiti@gmail.com>
2021-04-26 20:03:32 +1000

    scratch code

--- src/medium/CourseScheduleII210.java
+++ src/medium/CourseScheduleII210.java
@@ -1,11 +1,26 @@
 package medium;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CourseScheduleII210 {
+    private Object HashSet;
+
     public static void main(String[] args) {
         CourseScheduleII210 thisClass = new CourseScheduleII210();
         System.out.println (thisClass.findOrder(1, null));
     }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Arrays.sort(prerequisites, (a,b)->a[0] - b[0]);
+
+        Set<Integer> result = new HashSet<Integer>();
+        int pIndex = 0;
+        for (int i = 0; i < numCourses; i++){
+
+        }
+
 
         return null;
     }
---
+++ src/medium/WordSearchII212.java
@@ -0,0 +1,17 @@
+package medium;
+
+import java.util.List;
+
+public class WordSearchII212 {
+    public static void main(String[] args) {
+        WordSearchII212 thisClass = new WordSearchII212();
+        List<String> res = thisClass.findWords(
+                new char[][]{new char[]{'A','B'}, new char[]{'C','D'}}, new String[]{"ABDC", "f"});
+        System.out.println(res);
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+
+        return null;
+    }
+}
