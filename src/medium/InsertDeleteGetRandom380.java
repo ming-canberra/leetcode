31bc789757838c2e89b97f5b0381537b24d146ed
Ming <ming.edentiti@gmail.com>
2021-04-29 23:42:22 +1000

    get ready for next one

---
+++ src/medium/InsertDeleteGetRandom380.java
@@ -0,0 +1,37 @@
+package medium;
+
+public class InsertDeleteGetRandom380 {
+    public static void main(String[] args) {
+        InsertDeleteGetRandom380 thisClass = new InsertDeleteGetRandom380();
+    }
+
+    class RandomizedSet {
+        /**
+         * Initialize your data structure here.
+         */
+        public RandomizedSet() {
+
+        }
+
+        /**
+         * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+         */
+        public boolean insert(int val) {
+            return false;
+        }
+
+        /**
+         * Removes a value from the set. Returns true if the set contained the specified element.
+         */
+        public boolean remove(int val) {
+            return false;
+        }
+
+        /**
+         * Get a random element from the set.
+         */
+        public int getRandom() {
+            return 1;
+        }
+    }
+}
