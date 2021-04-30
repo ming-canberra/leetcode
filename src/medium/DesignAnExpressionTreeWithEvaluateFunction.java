d88bc114a422a80c6a07f243b34a6d9824860e6b
Ming <ming.edentiti@gmail.com>
2021-04-30 10:14:50 +1000

    set up for next one

---
+++ src/medium/DesignAnExpressionTreeWithEvaluateFunction.java
@@ -0,0 +1,10 @@
+package medium;
+
+import java.util.HashSet;
+
+public class DesignAnExpressionTreeWithEvaluateFunction {
+    public static void main(String[] args) {
+        DesignAnExpressionTreeWithEvaluateFunction thisClass = new DesignAnExpressionTreeWithEvaluateFunction();
+
+    }
+}
---
+++ src/medium/NumberOfProvinces547.java
@@ -0,0 +1,12 @@
+package medium;
+
+public class NumberOfProvinces547 {
+    public static void main(String[] args) {
+        NumberOfProvinces547 thisClass = new NumberOfProvinces547();
+
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        return 1;
+    }
+}
