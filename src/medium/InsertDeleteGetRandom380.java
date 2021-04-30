8ef0104da3c54a159a47f4c45091e9833baf5133
Ming <ming.edentiti@gmail.com>
2021-04-30 17:45:34 +1000

    need some luck to finish this

--- src/medium/InsertDeleteGetRandom380.java
+++ src/medium/InsertDeleteGetRandom380.java
@@ -1,37 +1,78 @@
 package medium;
 
+import java.util.*;
+
 public class InsertDeleteGetRandom380 {
     public static void main(String[] args) {
         InsertDeleteGetRandom380 thisClass = new InsertDeleteGetRandom380();
     }
 
     class RandomizedSet {
-        /**
-         * Initialize your data structure here.
-         */
+
+        // store
+        private List<Integer> list = new ArrayList<Integer>();
+
+        // from value to list index;
+        private Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        /** Initialize your data structure here. */
         public RandomizedSet() {
 
         }
 
-        /**
-         * Inserts a value to the set. Returns true if the set did not already contain the specified element.
-         */
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
         public boolean insert(int val) {
-            return false;
+            if (map.containsKey(val))
+            {
+                return false;
+            }
+            else
+            {
+                map.put(val, list.size());
+                list.add(list.size(), val);
+                return true;
+            }
         }
 
-        /**
-         * Removes a value from the set. Returns true if the set contained the specified element.
-         */
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
         public boolean remove(int val) {
-            return false;
+
+            if (map.containsKey(val))
+            {
+                int cIndex = map.get(val);
+
+                // copy the tail value to cIndex and chop off the tail integer
+                int endValue = list.get(list.size() - 1);
+                list.set(cIndex, endValue);
+
+                System.out.println(map.size());
+                System.out.println(endValue + " " + cIndex);
+                map.put(endValue, cIndex);
+
+                map.remove(val);
+                list.remove(list.size() - 1);
+                System.out.println("map size "+ map.size());
+                System.out.println("list size "+ list.size());
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        /**
-         * Get a random element from the set.
-         */
+        /** Get a random element from the set. */
         public int getRandom() {
-            return 1;
+            Random random = new Random();
+            return list.get(random.nextInt(list.size()));
         }
     }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
 }
