ad737b3c2324474a864a85e10128e6f3fbff6388
Ming <ming.edentiti@gmail.com>
2021-04-30 12:22:36 +1000

    need to do this again to make sure I understand. keynote: adjacency matrix graph

--- src/medium/NumberOfProvinces547.java
+++ src/medium/NumberOfProvinces547.java
@@ -7,6 +7,30 @@ public class NumberOfProvinces547 {
     }
 
     public int findCircleNum(int[][] isConnected) {
-        return 1;
+        int length = isConnected.length;
+        boolean[] visited = new boolean[length];
+        int count = 0;
+        for (int i = 0; i < length; i++){
+            if (!visited[i]){
+                dfs(isConnected, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int[][] graph, boolean[] visited, int i){
+
+        visited[i] = true;
+
+        for (int j = 0; j < visited.length; j++){
+            if (graph[i][j] == 1)
+            {
+                if (!visited[j])
+                {
+                    dfs(graph, visited, j);
+                }
+            }
+        }
     }
 }
