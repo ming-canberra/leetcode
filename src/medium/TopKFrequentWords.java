10ff7257b0cb315615fa39e1adb929577e5dbd9c
Ming <ming.edentiti@gmail.com>
2021-05-01 13:37:22 +1000

    prepare the next one

---
+++ src/medium/TopKFrequentWords.java
@@ -0,0 +1,17 @@
+package medium;
+
+import java.util.List;
+
+public class TopKFrequentWords {
+    public static void main(String[] args) {
+        TopKFrequentWords thisClass = new TopKFrequentWords();
+        List<String>   a = thisClass.topKFrequent(null, 1);
+
+        System.out.println(a);
+
+    }
+    public List<String> topKFrequent(String[] words, int k) {
+
+        return null;
+    }
+}
