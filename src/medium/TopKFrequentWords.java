5db81cb9f1f2e4a7f4a08bea5426fc0b3b941069
Ming <ming.edentiti@gmail.com>
2021-05-01 19:46:34 +1000

    not good enough. need to learn more about priority queue which could be used as heap somehow?

--- src/medium/TopKFrequentWords.java
+++ src/medium/TopKFrequentWords.java
@@ -1,6 +1,6 @@
 package medium;
 
-import java.util.List;
+import java.util.*;
 
 public class TopKFrequentWords {
     public static void main(String[] args) {
@@ -10,8 +10,27 @@ public class TopKFrequentWords {
         System.out.println(a);
 
     }
+    /**
+     time O(nlogn)
+     space O(n)
+     */
     public List<String> topKFrequent(String[] words, int k) {
 
-        return null;
+        List<String> result = new ArrayList<String>();
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        for (String s : words){
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for (String key : map.keySet())
+        {
+            result.add(key);
+        }
+
+        Collections.sort(result, (a, b)->  map.get(b) - map.get(a) == 0?a.compareTo(b):map.get(b) - map.get(a));
+
+        return result.subList(0, k);
     }
 }
