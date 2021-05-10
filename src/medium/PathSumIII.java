fce0e7fc3d4f00e7ddd71ccb203a49dfaba21112
Ming <ming.edentiti@gmail.com>
2021-05-10 17:30:56 +1000

    first time getting this accepted.
    O(N) and O(N)

--- src/medium/PathSumIII.java
+++ src/medium/PathSumIII.java
@@ -7,49 +7,41 @@ import java.util.List;
 public class PathSumIII {
     public static void main(String[] args) {
         PathSumIII thisClass = new PathSumIII();
-        int result = thisClass.pathSum(generteTree(), 1);
-        System.out.println(result);
     }
 
-    public static TreeNode generteTree(){
-        List<Integer> list = new ArrayList<Integer>();
-
-        Collections.addAll(list, 10,5,-3,3,2,null,11,3,-2,null,1);
-
-        TreeNode root = new TreeNode(list.get(0));
-        List<TreeNode> nodeList = new ArrayList<TreeNode>();
-        nodeList.add(root);
-        for (int i = 1; i < list.size(); i++){
-            if (i % 2 == 1){
-                // left
-                if (list.get(i) != null){
-                    TreeNode newNode = new TreeNode(list.get(i));
-                    nodeList.get((i - 1) /2).left = newNode;
-                    nodeList.add(newNode);
-                }
-                else {
-                    nodeList.add(null);
-                }
-            }
-            else{
-                // right
-                if (list.get(i) != null){
-                    TreeNode newNode = new TreeNode(list.get(i));
-                    nodeList.get((i - 1) /2).right = newNode;
-                    nodeList.add(newNode);
-                }
-                else {
-                    nodeList.add(null);
-                }
-            }
-        }
 
 
-        return root;
-    }
 
-    private static int _counter = 0;
+    /**
+     * space O(N)
+     * time O(N)
+     * */
+    private int result = 0;
     public int pathSum(TreeNode root, int targetSum) {
-        return 1;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        fun(root, targetSum, 0, map);
+        return result;
+    }
+    private void fun(TreeNode curr, int target, int sum, HashMap<Integer, Integer>map)
+    {
+        if (curr == null){
+            return;
+        }
+        sum += curr.val;
+        //check all paths ending with current node, except the one from current to root. See below for the edge case.
+        result += map.getOrDefault(sum - target, 0);
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+        if (curr.left != null){
+            fun(curr.left, target, sum, map);
+        }
+        if (curr.right != null){
+            fun(curr.right, target, sum, map);
+        }
+        //check the path from current to root
+        if (sum == target){
+            result++;
+        }
+        // we would like to reuse thie map,
+        map.put(sum, map.get(sum) - 1);
     }
 }
