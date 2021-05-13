99fb15c3bd6662ecbfb96d35874a3ed69d71c9b3
Ming <ming.edentiti@gmail.com>
2021-05-14 01:12:35 +1000

    is this my first hard one?

---
+++ src/hard/BinaryTreeMaximumPathSum124.java
@@ -0,0 +1,43 @@
+package hard;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeMaximumPathSum124 {
+    public static void main(String[] args) {
+        BinaryTreeMaximumPathSum124 thisClass = new BinaryTreeMaximumPathSum124();
+    }
+
+    private int result;
+    public int maxPathSum(TreeNode root) {
+        this.result = root.val;
+        maxPath(root);
+        return this.result;
+    }
+    // return the max path that I could contribute
+    private int maxPath(TreeNode node){
+
+        int tmp = node.val;
+        int lMaxPath = 0;
+        if (node.left != null){
+            lMaxPath = maxPath(node.left);
+            tmp = Math.max(tmp, node.val + lMaxPath);
+        }
+
+        int rMaxPath = 0;
+        if (node.right != null){
+            rMaxPath = maxPath(node.right);
+            tmp = Math.max(tmp, node.val + rMaxPath);
+        }
+
+        this.result = Math.max(this.result, tmp);
+
+        if (node.left != null && node.right != null){
+            this.result = Math.max(this.result, node.val + rMaxPath + lMaxPath);
+        }
+
+        return tmp;
+    }
+}
