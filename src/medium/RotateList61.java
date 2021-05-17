dfc3a650db6003abb49b9c7a4fff56a806630a92
Ming <ming.edentiti@gmail.com>
2021-05-17 17:27:17 +1000

    nailed it

---
+++ src/medium/RotateList61.java
@@ -0,0 +1,43 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotateList61 {
+    public static void main(String[] args) {
+        RotateList61 thisClass = new RotateList61();
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        int length = 0;
+        ListNode tmp = head;
+        while(tmp != null){
+            length++;
+            tmp = tmp.next;
+        }
+        int rotation = k % length;
+        if (rotation == 0){
+            return head;
+        }
+        else{
+            tmp = head;
+            ListNode tail1 = head;
+            ListNode tail2 = head.next;
+            ListNode head2 = head.next;
+            for (int i = 1; i < length - rotation; i++){
+                head2 = head2.next;
+                tail2 = tail2.next;
+                tail1 = tail1.next;
+            }
+            while(tail2.next != null){
+                tail2 = tail2.next;
+            }
+            tail1.next = null;
+            tail2.next = head;
+            return head2;
+        }
+    }
+}
