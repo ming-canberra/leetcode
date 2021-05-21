b5e2b3f2aaec4b9aa84e1c981e6e97c638a36824
Ming <ming.edentiti@gmail.com>
2021-05-21 16:00:23 +1000

    did it in time

--- src/easy/SearchInsertPosition35.java
+++ src/easy/SearchInsertPosition35.java
@@ -1,15 +1,34 @@
 package easy;
 
-import java.util.Arrays;
-
 public class SearchInsertPosition35 {
     public static void main(String[] args) {
         SearchInsertPosition35 thisClass = new SearchInsertPosition35();
-
-        System.out.println(thisClass.searchInsert(new int[] {1,2,3,4,5,10}, 2));
     }
-    //do binary search
+
     public int searchInsert(int[] nums, int target) {
-        return 123;
+        int l = 0;
+        int r = nums.length;
+        while(l < r){
+            int m = l + (r - l) / 2;
+            if (target == nums[m]){
+                return m;
+            }
+            else if (target > nums[m]){
+                l = m + 1;
+            }
+            else{
+                r = m;
+            }
+        }
+
+        if (l == nums.length){
+            return l;
+        }
+        else if (target > nums[l]){
+            return l + 1;
+        }
+        else{
+            return l;
+        }
     }
 }
