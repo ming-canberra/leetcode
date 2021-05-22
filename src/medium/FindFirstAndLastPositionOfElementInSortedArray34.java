a00c43467bebba16f1eea03685ad0589116d2e3d
Ming <ming.edentiti@gmail.com>
2021-05-22 19:37:02 +1000

    nailed it. pay attention to ending part, then I should be good.

--- src/medium/FindFirstAndLastPositionOfElementInSortedArray34.java
+++ src/medium/FindFirstAndLastPositionOfElementInSortedArray34.java
@@ -6,7 +6,42 @@ public class FindFirstAndLastPositionOfElementInSortedArray34 {
         System.out.println (thisClass.searchRange(new int[]{5,7,7,8,8,10}, 8));
     }
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[]{-1,-1};
-        return ans;
+
+        int startResult = -1;
+
+        int l = -1;
+        int r = nums.length;
+
+        while (l + 1 < r){
+            int m = l + (r - l) / 2;
+
+            if(nums[m] < target){
+                l = m;
+            }
+            else{
+                r = m;
+            }
+        }
+        if (r == nums.length || nums[r] != target){
+            return new int[]{-1, -1};
+        }
+        startResult = r;
+
+        int endResult = 0;
+        l = r -1;
+        r = nums.length;
+
+        while (l + 1 < r){
+            int m = l + (r - l) / 2;
+            if(nums[m] <= target){
+                l = m;
+            }
+            else{
+                r = m;
+            }
+        }
+        endResult = l;
+
+        return new int[]{startResult, endResult};
     }
 }
