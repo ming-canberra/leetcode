df3aaacab67f149f094d8c30bd4a3a41f20a1aa8
Ming <ming.edentiti@gmail.com>
2021-05-25 02:10:04 +1000

    old implementation

--- src/medium/DailyTemperatures739.java
+++ src/medium/DailyTemperatures739.java
@@ -1,8 +1,6 @@
 package medium;
 
-import easy.ListNode;
-
-import java.util.Stack;
+import java.util.*;
 
 public class DailyTemperatures739 {
     public static void main(String[] args) {
@@ -10,7 +8,17 @@ public class DailyTemperatures739 {
 
         thisClass.dailyTemperatures(null);
     }
-    public int[] dailyTemperatures(int[] t) {
-        return null;
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] dist = new int[n];
+        Stack<Integer> indexs = new Stack<>();
+        for (int curIndex = 0; curIndex < n; curIndex++) {
+            while (!indexs.isEmpty() && temperatures[curIndex] > temperatures[indexs.peek()]) {
+                int preIndex = indexs.pop();
+                dist[preIndex] = curIndex - preIndex;
+            }
+            indexs.add(curIndex);
+        }
+        return dist;
     }
 }
