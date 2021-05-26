ec9f545377cfbb6cc71f742e03f067548ba38f7e
Ming <ming.edentiti@gmail.com>
2021-05-26 19:55:43 +1000

    solution 2

--- src/medium/MinimumRemoveToMakeValidParentheses1249.java
+++ src/medium/MinimumRemoveToMakeValidParentheses1249.java
@@ -1,6 +1,6 @@
 package medium;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class MinimumRemoveToMakeValidParentheses1249 {
     public static void main(String[] args) {
@@ -41,6 +41,56 @@ public class MinimumRemoveToMakeValidParentheses1249 {
                 result.append(sb.charAt(i));
             }
             return result.reverse().toString();
+        }
+    }
+
+    /**
+     * time O(N)
+     * space O(N)
+     * */
+    public String minRemoveToMakeValid2(String s) {
+        Stack<CharIndex> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++){
+            char curChar = s.charAt(i);
+
+            if (curChar == '('){
+                stack.push(new CharIndex(curChar, i));
+            }
+            else if (curChar == ')'){
+
+                if (stack.isEmpty()){
+                    stack.push(new CharIndex(curChar, i));
+                }
+                else{
+                    CharIndex tmp = stack.peek();
+                    if (tmp.c == '('){
+                        stack.pop();
+                    }
+                    else{
+                        stack.push(new CharIndex(curChar, i));
+                    }
+                }
+            }
+        }
+        Set<Integer> indexSet = new HashSet<>();
+        while (!stack.isEmpty()){
+            indexSet.add(stack.pop().index);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++){
+            char curChar = s.charAt(i);
+            if (!indexSet.contains(i)){
+                sb.append(curChar);
+            }
+        }
+        return sb.toString();
+    }
+    class CharIndex{
+        char c;
+        int index;
+        CharIndex (char cValue, int indexValue){
+            c = cValue;
+            index = indexValue;
         }
     }
 }
