403cb80c262bfe17ad979b19331f821eefdee845
Ming <ming.edentiti@gmail.com>
2021-05-28 02:53:38 +1000

    second attempt, referred to others' solution

--- src/easy/ImplementQueueUsingStacks232.java
+++ src/easy/ImplementQueueUsingStacks232.java
@@ -47,4 +47,45 @@ public class ImplementQueueUsingStacks232 {
         }
     }
 
+    /**
+     * amortised O(1)
+     * */
+    class MyQueue2 {
+
+        private Stack<Integer> pushStack;
+        private Stack<Integer> popStack;
+
+        /** Initialize your data structure here. */
+        public MyQueue2() {
+            pushStack = new Stack<>();
+            popStack = new Stack<>();
+        }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            pushStack.push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            peek();
+            return popStack.pop();
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            if(popStack.isEmpty()){
+                while(!pushStack.isEmpty()){
+                    popStack.push(pushStack.pop());
+                }
+            }
+
+            return popStack.peek();
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return pushStack.isEmpty() && popStack.isEmpty();
+        }
+    }
 }
