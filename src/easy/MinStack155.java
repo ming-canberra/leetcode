8b9466cbcb143808f13953fb1a4e011360d4d60a
Ming <ming.edentiti@gmail.com>
2021-05-27 16:41:21 +1000

    my previous solution is basically wrong.

--- src/easy/MinStack155.java
+++ src/easy/MinStack155.java
@@ -36,4 +36,37 @@ public class MinStack155 {
     public int getMin() {
         return _min;
     }
+
+
+
+    class MinStack {
+
+        /** initialize your data structure here. */
+        private Stack<int[]> stack;
+        public MinStack() {
+            stack = new Stack<>();
+        }
+
+        public void push(int val) {
+            if (stack.isEmpty()){
+                stack.push(new int[]{val, val});
+            }
+            else{
+                int min = Math.min(stack.peek()[1], val);
+                stack.push(new int[]{val, min});
+            }
+        }
+
+        public void pop() {
+            stack.pop();
+        }
+
+        public int top() {
+            return stack.peek()[0];
+        }
+
+        public int getMin() {
+            return stack.peek()[1];
+        }
+    }
 }
