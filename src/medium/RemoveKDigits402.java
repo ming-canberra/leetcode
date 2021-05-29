d780719d431321a7d7fef5c0952a83fb91184d1d
Ming <ming.edentiti@gmail.com>
2021-05-29 21:13:11 +1000

    this is the second solution. seems fine.

--- src/medium/RemoveKDigits402.java
+++ src/medium/RemoveKDigits402.java
@@ -1,68 +1,115 @@
 package medium;
 
+import java.util.*;
+
 public class RemoveKDigits402 {
-    StringBuilder sb = new StringBuilder();
 
-    public String removeKdigits(String num, int k) {
-        recur(num, k);
-        String result = sb.toString();
-        if (result.isEmpty()){
-            return "0";
-        }
-
-        // to remove leading 0
-        int i = -1;
-        while(i + 1 < result.length() && result.charAt(i + 1) == '0'){
-            i++;
-        }
-        if (i + 1 < result.length()){
-            result = result.substring(i + 1);
-            return result;
-        }
-        else{
-            return "0";
+    /**
+     * O(N)
+     * O(N)
+     * */
+    class Solution1 {
+        public String removeKdigits(String num, int k) {
+            Stack<Character> stack = new Stack<>();
+            char[] charArray = num.toCharArray();
+            stack.push(charArray[0]);
+            for (int i = 1; i < num.length(); i++){
+                while(!stack.isEmpty() && k >0){
+                    char top = stack.peek();
+                    if (top > charArray[i]){
+                        k--;
+                        stack.pop();
+                    }
+                    else{
+                        break;
+                    }
+                }
+                if (charArray[i] == '0' && stack.isEmpty()){
+                    // do not need leading 0
+                }
+                else{
+                    stack.push(charArray[i]);
+                }
+            }
+            while (k > 0 && !stack.isEmpty()){
+                k--;
+                stack.pop();
+            }
+            if (stack.isEmpty()){
+                return "0";
+            }
+            StringBuilder sb = new StringBuilder();
+            while (!stack.isEmpty()){
+                sb.append(stack.pop());
+            }
+            return sb.reverse().toString();
         }
     }
 
-    public void recur(String num, int k) {
-        // search for the smallest during the first k + 1 char
-        if (k == 0){
-            sb.append(num);
-            return;
-        }
-        int length = num.length();
-        if (k >= length) {
-            sb.append("");
-            return;
-        }
-        int minIndex = 0; // first index of min if there are multiple
-        int min = 9;
-        for (int i = 0; i <= k; i++){
-            if (num.charAt(i) == '0'){
-                if (i + 1 == length){
-                    sb.append("0");
-                    return;
-                }
-                else{
-                    sb.append("0");
-                    recur(num.substring(i + 1), k - i);
-                    return;
-                }
+    /**
+     * O(N*k)
+     * O(N)
+     * */
+    class Solution2 {
+        StringBuilder sb = new StringBuilder();
+
+        public String removeKdigits(String num, int k) {
+            recur(num, k);
+            String result = sb.toString();
+            if (result.isEmpty()) {
+                return "0";
             }
-            else{
-                if (num.charAt(i) - '0' < min){
-                    min = num.charAt(i) - '0';
-                    minIndex = i;
-                }
+
+            // to remove leading 0
+            int i = -1;
+            while (i + 1 < result.length() && result.charAt(i + 1) == '0') {
+                i++;
+            }
+            if (i + 1 < result.length()) {
+                result = result.substring(i + 1);
+                return result;
+            } else {
+                return "0";
             }
         }
 
-        if (minIndex + 1 == length){
-            sb.append(min);
-        }
-        else{
-            sb.append(min);
-            recur(num.substring(minIndex + 1), k - minIndex);
+        public void recur(String num, int k) {
+            // search for the smallest during the first k + 1 char
+            if (k == 0) {
+                sb.append(num);
+                return;
+            }
+            int length = num.length();
+            if (k >= length) {
+                sb.append("");
+                return;
+            }
+            int minIndex = 0; // first index of min if there are multiple
+            int min = 9;
+            for (int i = 0; i <= k; i++) {
+                if (num.charAt(i) == '0') {
+                    if (i + 1 == length) {
+                        sb.append("0");
+                        return;
+                    } else {
+                        sb.append("0");
+                        recur(num.substring(i + 1), k - i);
+                        return;
+                    }
+                } else {
+                    if (num.charAt(i) - '0' < min) {
+                        min = num.charAt(i) - '0';
+                        minIndex = i;
+                    }
+                }
+            }
+
+            if (minIndex + 1 == length) {
+                sb.append(min);
+            } else {
+                sb.append(min);
+                recur(num.substring(minIndex + 1), k - minIndex);
+            }
         }
     }
 }
