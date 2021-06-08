da687121db412bbc2d9d198b68223146e138b601
Ming <ming.edentiti@gmail.com>
2021-06-09 03:11:56 +1000

    it took me a while, but did it eventually. Second time doing it DFS recursive

--- src/medium/NumberOfIslands.java
+++ src/medium/NumberOfIslands.java
@@ -38,4 +38,39 @@ public class NumberOfIslands {
         }
         return false;
     }
+
+
+    class Solution {
+
+        public int numIslands(char[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            boolean[][] visited = new boolean[m][n];
+            int count = 0;
+            for (int i = 0; i < m; i++){
+                for (int j = 0; j < n; j++){
+                    if (!visited[i][j] && grid[i][j] == '1'){
+                        count++;
+                        dfs(grid, visited, i, j, m, n);
+                    }
+                }
+            }
+            return count;
+        }
+        /**
+         * mark all adjacent grid as visited. so that only unvisited 1 can be counted.
+         */
+        private void dfs(char[][]grid, boolean[][] visited, int i, int j, int m, int n){
+            if (i >= 0 && i < m && j >= 0 && j < n && !visited[i][j]){
+                visited[i][j] = true;
+                if (grid[i][j] == '1'){
+                    // is island
+                    dfs(grid, visited, i + 1, j, m, n);
+                    dfs(grid, visited, i - 1, j, m, n);
+                    dfs(grid, visited, i, j + 1, m, n);
+                    dfs(grid, visited, i, j - 1, m, n);
+                }
+            }
+        }
+    }
 }
