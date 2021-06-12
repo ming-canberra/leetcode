469d8a840a257bd5da7c013f9a45e9d16aceda4f
Ming <ming.edentiti@gmail.com>
2021-06-12 16:20:15 +1000

    second time doing it by referring to standard solution. I spend too much time debugging, and did not finish within 45 mins.

--- src/medium/LRUCache146.java
+++ src/medium/LRUCache146.java
@@ -3,13 +3,94 @@ package medium;
 import java.util.*;
 
 public class LRUCache146 {
+
     class LRUCache {
+        int capacity;
+        HashMap<Integer, DLinkedList> map = new HashMap<>(); // head to store latest node
+        DLinkedList preHead;
+        DLinkedList postTail;
+        public LRUCache(int capacity) {
+            this.capacity = capacity;
+            preHead = new DLinkedList(0,0,null,null);
+            postTail = new DLinkedList(0,0,null,null);
+            preHead.next = postTail;
+            postTail.prev = preHead;
+        }
+        public int get(int key) {
+            if (map.containsKey(key)){
+                DLinkedList node = map.get(key);
+                remove(node);
+                addToFront(node);
+                return node.value;
+            }
+            return -1;
+        }
+        public void put(int key, int value) {
+            if (map.containsKey(key)){
+                DLinkedList node = map.get(key);
+                node.value = value;
+                remove(node);
+                addToFront(node);
+            }
+            else{
+                DLinkedList node = new DLinkedList(key, value, null, null);
+                if (map.size() >= capacity){
+                    int leastUsed = removeLast();
+                    map.remove(leastUsed);
+                }
+                map.put(key, node);
+                addToFront(node);
+            }
+        }
+        private void remove(DLinkedList node){
+            DLinkedList prev = node.prev;
+            DLinkedList next = node.next;
+            prev.next = next;
+            next.prev = prev;
+        }
+        private int removeLast(){
+            DLinkedList tail = postTail.prev;
+            DLinkedList preTail = tail.prev;
+            preTail.next = postTail;
+            postTail.prev = preTail;
+            return tail.key;
+        }
+        private void addToFront(DLinkedList node){
+            DLinkedList head = preHead.next;
+            preHead.next = node;
+            node.prev = preHead;
+            node.next = head;
+            head.prev = node;
+        }
+        class DLinkedList{
+            DLinkedList prev;
+            DLinkedList next;
+            Integer value;
+            Integer key;
+            public DLinkedList(Integer inputKey, Integer inputValue, DLinkedList prev, DLinkedList next){
+                this.value = inputValue;
+                this.key = inputKey;
+                this.prev = prev;
+                this.next = next;
+            }
+        }
+    }
+
+    /**
+     * Your LRUCache object will be instantiated and called as such:
+     * LRUCache obj = new LRUCache(capacity);
+     * int param_1 = obj.get(key);
+     * obj.put(key,value);
+     */
+
+
+    class LRUCache1 {
         private Map<Integer, Set<Integer>> frequencyToKeys = new HashMap<Integer, Set<Integer>>();
         private Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         private Map<Integer, Integer> keyToFrequency = new HashMap<Integer, Integer>();
         private int minFrequency = 0;
         private int mapCapacity = 0;
-        public LRUCache(int capacity) {
+        public LRUCache1(int capacity) {
             mapCapacity = capacity;
         }
         /**
