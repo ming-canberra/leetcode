0f7cd246b0728d66c1ba3e08a2b7aba9249f059c
Ming <ming.edentiti@gmail.com>
2021-06-14 22:06:39 +1000

    nailed it. However I need to remember random.nextInt(size()) method of Random class.

--- src/medium/InsertDeleteGetRandom380.java
+++ src/medium/InsertDeleteGetRandom380.java
@@ -68,11 +68,46 @@ public class InsertDeleteGetRandom380 {
         }
     }
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
+
+    class RandomizedSet2 {
+
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>(); //value to index at list
+        Random random  = new Random();
+
+        /** Initialize your data structure here. */
+        public RandomizedSet2() {
+
+        }
+
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+        public boolean insert(int val) {
+            if (map.containsKey(val)){
+                return false;
+            }
+            map.put(val, list.size());
+            list.add(val);
+            return true;
+        }
+
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
+        public boolean remove(int val) {
+            if (!map.containsKey(val)){
+                return false;
+            }
+            int valIndex = map.get(val);
+            int valueAtEnd = list.get(list.size() - 1);
+            map.put(valueAtEnd, valIndex);
+            list.set(valIndex, valueAtEnd);
+            list.remove(list.size() - 1);
+            map.remove(val);
+            return true;
+        }
+
+        /** Get a random element from the set. */
+        public int getRandom() {
+            return list.get(random.nextInt(list.size()));
+        }
+    }
+
 }
