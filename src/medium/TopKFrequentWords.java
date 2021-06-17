05be669085a674fc12d4db4b6b5d41a36ae6a032
Ming <ming.edentiti@gmail.com>
2021-06-18 02:05:13 +1000

    I am ok with heap now

--- src/medium/TopKFrequentWords.java
+++ src/medium/TopKFrequentWords.java
@@ -33,4 +33,46 @@ public class TopKFrequentWords {
 
         return result.subList(0, k);
     }
+
+    class Solution {
+        public List<String> topKFrequent(String[] words, int k) {
+            //low heap and high alphabetical
+            PriorityQueue<Pair> min = new PriorityQueue<Pair>(new MyCom());
+            Map<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < words.length; i++){
+                map.put (words[i], map.getOrDefault(words[i], 0) + 1);
+            }
+            for (String key : map.keySet()){
+                min.add(new Pair(key, map.get(key)));
+                if (min.size() > k){
+                    min.poll();
+                }
+            }
+            List<String> result = new ArrayList<>();
+            for (int i = 0; i < k; i++){
+                result.add("");
+            }
+            for (int i = k - 1; i >= 0; i--){
+                result.set(i, min.poll().word);
+            }
+            return result;
+        }
+        class Pair{
+            int frequency;
+            String word;
+            Pair(String w, int f){
+                frequency = f;
+                word = w;
+            }
+        }
+        class MyCom implements Comparator<Pair>{
+            @Override
+            public int compare(Pair a, Pair b){
+                if (a.frequency == b.frequency){
+                    return b.word.compareTo(a.word);
+                }
+                return a.frequency - b.frequency;
+            }
+        }
+    }
 }
