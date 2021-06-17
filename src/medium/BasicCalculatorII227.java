90131f64700efad6e1e151be5f1394d0929efe44
Ming <ming.edentiti@gmail.com>
2021-06-17 13:55:54 +1000

    the implementation is quite tricky while the algo is easy.
    inside each iteration, use the last operation to decide what to do with the latest integer, then assign the current operation for use in next iterations.

--- src/medium/BasicCalculatorII227.java
+++ src/medium/BasicCalculatorII227.java
@@ -3,14 +3,43 @@ package medium;
 import java.util.*;
 
 public class BasicCalculatorII227 {
-    public static void main(String[] args) {
-        BasicCalculatorII227 thisClass = new BasicCalculatorII227();
-        int  a = thisClass.calculate("");
+    class Solution {
+        public int calculate(String s) {
+            Stack<Integer> stack = new Stack<>();
+            //the key is to apply previous op first, then set the current op value to the previous op represented by the char in current iteration
+            char preOp = '+';
+            int curInt = 0;
+            s = s.replace(" ", "");
+            for (int i = 0; i < s.length(); i++){
+                char curChar = s.charAt(i);
+                if (Character.isDigit(curChar)){
+                    curInt = 10 * curInt + curChar - '0';
+                }
+                if (!Character.isDigit(curChar) || i == s.length() - 1){
+                    if (preOp == '+'){
+                        stack.push(curInt);
+                    }
+                    else if (preOp == '-'){
+                        stack.push(-curInt);
+                    }
+                    else if (preOp == '*'){
+                        stack.push(curInt * stack.pop());
+                    }
+                    else if (preOp == '/'){
+                        stack.push(stack.pop() / curInt);
+                    }
+                    preOp = curChar;
+                    curInt = 0;
+                }
+            }
+            int result = 0;
+            while (!stack.isEmpty()){
+                result += stack.pop();
+            }
 
-        System.out.println(a);
+            return result;
+        }
+    }
 
-    }
-    public int calculate(String s) {
-        return 123;
-    }
+
 }
