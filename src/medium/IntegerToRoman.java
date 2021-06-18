a4d3d9e4249d4c010e2a992d8236937b3f0e76d2
Ming <ming.edentiti@gmail.com>
2021-06-18 12:31:59 +1000

    referred to the standard solution, which is much shorter in terms of coding. greedy if anything.

--- src/medium/IntegerToRoman.java
+++ src/medium/IntegerToRoman.java
@@ -1,12 +1,27 @@
 package medium;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class IntegerToRoman {
-    public static void main(String[] args) {
-        IntegerToRoman thisClass = new IntegerToRoman();
 
+    class Solution {
+        public String intToRoman(int num) {
+            int[] val = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            String[] str = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < val.length; i++){
+                while(num - val[i] >= 0){
+                    num -= val[i];
+                    sb.append(str[i]);
+                }
+                if (num == 0){
+                    break;
+                }
+            }
+            return sb.toString();
+        }
     }
+
     public String intToRoman(int num) {
 
         StringBuilder sb = new StringBuilder();
