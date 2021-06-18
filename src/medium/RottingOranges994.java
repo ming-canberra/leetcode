4b6bac3e4cffa8e562f08c3342affd9f3634a3df
Ming <ming.edentiti@gmail.com>
2021-06-18 17:38:01 +1000

    standard graph BFS using queue

--- src/medium/RottingOranges994.java
+++ src/medium/RottingOranges994.java
@@ -68,4 +68,54 @@ public class RottingOranges994 {
 
         return minute;
     }
+
+    class Solution {
+        int freshCount = 0;
+        int result = 0;
+        public int orangesRotting(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            Queue<int[]> queue = new LinkedList<>();
+            boolean[][] isFresh = new boolean[m][n];
+            for (int i = 0; i < m; i++){
+                for (int j = 0; j < n; j++){
+                    if (grid[i][j] == 1){
+                        freshCount++;
+                        isFresh[i][j] = true;
+                    }
+                    else if (grid[i][j] == 2){
+                        queue.add(new int[]{i, j});
+                    }
+                }
+            }
+            if (freshCount == 0){
+                return 0;
+            }
+            while(!queue.isEmpty()){
+                int size = queue.size();
+                for (int i = 0; i < size; i++){
+                    int[] top = queue.poll();
+                    checkNeighbours(queue, isFresh, top[0] + 1, top[1], m, n);
+                    checkNeighbours(queue, isFresh, top[0] - 1, top[1], m, n);
+                    checkNeighbours(queue, isFresh, top[0], top[1] + 1, m, n);
+                    checkNeighbours(queue, isFresh, top[0], top[1] - 1, m, n);
+                }
+                result++;
+                if (freshCount == 0){
+                    return result;
+                }
+            }
+            return -1;
+        }
+
+        private void checkNeighbours(Queue<int[]> queue, boolean[][] isFresh, int i, int j, int m, int n){
+            if (i >= 0 && i < m && j >=0 && j < n){
+                if (isFresh[i][j]){
+                    isFresh[i][j] = false;
+                    queue.add(new int[]{i, j});
+                    freshCount--;
+                }
+            }
+        }
+    }
 }
