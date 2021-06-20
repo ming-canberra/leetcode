9e84ff29acd5906ba49595da16db5995ba5bdb4a
Ming <ming.edentiti@gmail.com>
2021-06-21 02:23:00 +1000

    nailed it within 40 mins

--- src/medium/DesignSnakeGame353.java
+++ src/medium/DesignSnakeGame353.java
@@ -77,9 +77,77 @@ public class DesignSnakeGame353 {
         }
     }
 
-/**
- * Your SnakeGame object will be instantiated and called as such:
- * SnakeGame obj = new SnakeGame(width, height, food);
- * int param_1 = obj.move(direction);
- */
+    class SnakeGame2 {
+
+        /** Initialize your data structure here.
+         @param width - screen width
+         @param height - screen height
+         @param food - A list of food positions
+         E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
+
+        private int[] headPos = new int[]{0, 0};
+        private Queue<int[]> bodyQ = new LinkedList<>();
+        private Set<Integer> bodyS = new HashSet<>();
+        private int foodIndex = 0;
+        private int foodCount;
+        private int score = 0;
+        private int w;
+        private int h;
+        private int[][] foods;
+        public SnakeGame2(int width, int height, int[][] food) {
+            w = width;
+            h = height;
+            foods = food;
+            foodCount = food.length;
+        }
+
+        /** Moves the snake.
+         @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down
+         @return The game's score after the move. Return -1 if game over.
+         Game over when snake crosses the screen boundary or bites its body. */
+        public int move(String direction) {
+            int prevR = headPos[0];
+            int prevC = headPos[1];
+            if (direction.equals("U")){
+                headPos[0] += -1;
+            }
+            else if (direction.equals("L")){
+                headPos[1] += -1;
+            }
+            else if (direction.equals("R")){
+                headPos[1] += 1;
+            }
+            else if (direction.equals("D")){
+                headPos[0] += 1;
+            }
+            //goes out of bound
+            if (headPos[0] < 0 || headPos[0] >= h || headPos[1] < 0 || headPos[1] >= w){
+                return -1;
+            }
+            //foodie
+            if(foodIndex < foodCount){
+                int[] food = foods[foodIndex];
+                if (food[0] == headPos[0] && food[1] == headPos[1]){
+                    bodyQ.add(new int[]{prevR, prevC});
+                    bodyS.add(prevR * w + prevC);
+                    foodIndex++;
+                    score++;
+                    return score;
+                }
+            }
+            if (!bodyQ.isEmpty()){
+                int[] top = bodyQ.poll();
+                bodyS.remove(top[0] * w + top[1]);
+                //collision
+                if (bodyS.contains(headPos[0] * w + headPos[1] )){
+                    return -1;
+                }
+                bodyQ.add(new int[]{prevR, prevC});
+                bodyS.add(prevR * w + prevC);
+            }
+            return score;
+        }
+    }
+
+
 }
