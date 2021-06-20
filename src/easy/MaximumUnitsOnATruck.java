ed52d88d5848233b8844df4b17e661bdf8d0ab51
Ming <ming.edentiti@gmail.com>
2021-06-20 14:09:46 +1000

    easy sort and be greedy

--- src/easy/MaximumUnitsOnATruck.java
+++ src/easy/MaximumUnitsOnATruck.java
@@ -25,4 +25,22 @@ public class MaximumUnitsOnATruck {
         }
         return result;
     }
+
+    class Solution {
+        public int maximumUnits(int[][] boxTypes, int truckSize) {
+            Arrays.sort(boxTypes, (a, b)-> b[1] - a[1]);
+            int result = 0;
+            for (int i = 0; i < boxTypes.length; i++){
+                if (truckSize - boxTypes[i][0] >= 0){
+                    truckSize -= boxTypes[i][0];
+                    result += boxTypes[i][0] * boxTypes[i][1];
+                }
+                else{
+                    result += truckSize * boxTypes[i][1];
+                    break;
+                }
+            }
+            return result;
+        }
+    }
 }
