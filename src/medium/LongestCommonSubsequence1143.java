9f31347e4cb8b7d0f866e81b75e3a42033e789be
Ming <ming.edentiti@gmail.com>
2021-07-03 23:42:09 +1000

    did not work it out myself, need to do it again

--- src/medium/LongestCommonSubsequence1143.java
+++ src/medium/LongestCommonSubsequence1143.java
@@ -1,14 +1,22 @@
 package medium;
 
 public class LongestCommonSubsequence1143 {
-    public static void main(String[] args) {
-        LongestCommonSubsequence1143 thisClass = new LongestCommonSubsequence1143();
-        int res = thisClass.longestCommonSubsequence("123123", "sdfsfd");
-        System.out.println(res);
-    }
-
-    public int longestCommonSubsequence(String text1, String text2)
-    {
-        return 123;
+    class Solution {
+        public int longestCommonSubsequence(String text1, String text2) {
+            int m = text1.length();
+            int n = text2.length();
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 1; i < m + 1; i++){
+                for (int j = 1; j < n + 1; j++){
+                    if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    }
+                    else{
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            return dp[m][n];
+        }
     }
 }
