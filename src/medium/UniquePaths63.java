8e3671dbe8eb148c728528d316dbbabd3f418f87
Ming <ming.edentiti@gmail.com>
2021-07-03 19:08:20 +1000

    nailed it, bug free the first time.

---
+++ src/medium/UniquePaths63.java
@@ -0,0 +1,30 @@
+package medium;
+
+public class UniquePaths63 {
+    class Solution {
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            int m = obstacleGrid.length;
+            int n = obstacleGrid[0].length;
+            if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1){
+                return 0;
+            }
+            int[][]dp = new int[m + 1][n + 1];
+            for (int i = 1; i < m + 1; i++){
+                for (int j = 1; j < n + 1; j++){
+                    if (i == 1 && j == 1){
+                        dp[i][j] = 1;
+                    }
+                    else{
+                        if(obstacleGrid[i - 1][j - 1] == 1){
+                            dp[i][j] = 0;
+                        }
+                        else{
+                            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                        }
+                    }
+                }
+            }
+            return dp[m][n];
+        }
+    }
+}
