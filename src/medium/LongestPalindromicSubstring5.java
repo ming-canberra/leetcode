57284432d37eb0bcf2a8ed3a467e676d395a9557
Ming <ming.edentiti@gmail.com>
2021-07-04 22:17:55 +1000

    did not come up with it by myself. this is a DP solution

--- src/medium/LongestPalindromicSubstring5.java
+++ src/medium/LongestPalindromicSubstring5.java
@@ -34,4 +34,44 @@ public class LongestPalindromicSubstring5 {
             }
         }
     }
+
+    class Solution2 {
+        public String longestPalindrome(String s) {
+            int n = s.length();
+            boolean dp[][] = new boolean[n][n]; //s.substring(i, j + 1) is palindrome
+            int maxLength = 1;
+            int i = 0;
+            int j = 0;
+            for (int r = n - 1; r >= 0; r--){
+                for (int c = r; c < n; c++){
+                    if (r == c){
+                        dp[r][c] = true;
+                    }
+                    else{
+                        if (s.charAt(r) == s.charAt(c)){
+                            if (r + 1 == c){
+                                dp[r][c] = true;
+                                if (2 > maxLength ){
+                                    maxLength = 2;
+                                    i = r;
+                                    j = c;
+                                }
+                            }
+                            else{
+                                if (dp[r + 1][c - 1]){
+                                    dp[r][c] = true;
+                                    if (c + 1 - r > maxLength ){
+                                        maxLength = c + 1 - r;
+                                        i = r;
+                                        j = c;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return s.substring(i, j + 1);
+        }
+    }
 }
