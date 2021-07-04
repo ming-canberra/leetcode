a09af492eb9b4a48151ba1208ed71f46746283c5
Ming <ming.edentiti@gmail.com>
2021-07-04 19:25:09 +1000

    nailed it, bug free

--- src/medium/RangeSumQuery2DImmutable304.java
+++ src/medium/RangeSumQuery2DImmutable304.java
@@ -27,4 +27,25 @@ public class RangeSumQuery2DImmutable304 {
 
         return dp[row2 + 1][col2 + 1] + dp[row1 ][col1 ] - dp[row1 ][col2 +1 ] - dp[row2 + 1][col1];
     }
+
+    class NumMatrix {
+        int[][] dp;
+        int m;
+        int n;
+        public NumMatrix(int[][] matrix) {
+            m = matrix.length;
+            n = matrix[0].length;
+            dp = new int[m + 1][n + 1];
+            for (int i = 0; i < m + 1; i++){
+                for (int j = 0; j < n + 1; j++){
+                    if (i != 0 && j != 0){
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            return dp[1 + row2][1 + col2] + dp[row1][col1] - dp[1 + row2][col1] - dp[row1][1 + col2];
+        }
+    }
 }
