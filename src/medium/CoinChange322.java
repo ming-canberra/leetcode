a0554367c82d921808d039f2f3156f323f853cfd
Ming <ming.edentiti@gmail.com>
2021-07-08 01:17:41 +1000

    need to do this again

--- src/medium/CoinChange322.java
+++ src/medium/CoinChange322.java
@@ -36,4 +36,32 @@ public class CoinChange322 {
 
         return dp[amount];
     }
+
+    class Solution {
+        public int coinChange(int[] coins, int amount) {
+            Arrays.sort(coins);
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp, -1);
+            dp[0] = 0;
+            for (int i = 1; i < dp.length; i++){
+                Integer min = null;
+                for (int j = 0; j < coins.length; j++){
+                    if (i - coins[j] >= 0){
+                        if (dp[i - coins[j]] != -1){
+                            if (min == null){
+                                min = dp[i - coins[j]];
+                            }
+                            else{
+                                min = Math.min(min, dp[i - coins[j]]);
+                            }
+                        }
+                    }
+                }
+                if (min != null){
+                    dp[i] = min + 1;
+                }
+            }
+            return dp[amount];
+        }
+    }
 }
