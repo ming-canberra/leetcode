2c67af3b971baf623dcdd7fb9abf03c07c18aec8
Ming <ming.edentiti@gmail.com>
2021-07-08 15:32:14 +1000

    dp, bug free

--- src/medium/PredictTheWinner486.java
+++ src/medium/PredictTheWinner486.java
@@ -39,4 +39,22 @@ public class PredictTheWinner486 {
             return memo[start][end];
         }
     }
+
+    class Solution2 {
+        public boolean PredictTheWinner(int[] nums) {
+            int n = nums.length;
+            int[][] dp = new int[n][n];
+            for (int i = n - 1; i >= 0; i--){
+                for (int j = i; j < n; j++){
+                    if (i == j){
+                        dp[i][j] = nums[i];
+                    }
+                    else{
+                        dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+                    }
+                }
+            }
+            return  dp[0][n - 1] >= 0;
+        }
+    }
 }
