544467ac7601c79abd7e845a97018a09e3351743
Ming <ming.edentiti@gmail.com>
2021-07-10 15:01:48 +1000

    classic and easy dp

--- src/medium/HouseRobber198.java
+++ src/medium/HouseRobber198.java
@@ -1,12 +1,6 @@
 package medium;
 
 public class HouseRobber198 {
-    public static void main(String[] args) {
-        HouseRobber198 thisClass = new HouseRobber198();
-
-        int res = thisClass.rob(new int[]{2,7,9,3,1});
-        System.out.println(res);
-    }
     public int rob(int[] nums) {
         int [] helperArray = new int[nums.length];
         helperArray[0] = nums[0];
@@ -19,5 +13,21 @@ public class HouseRobber198 {
             }
         }
         return helperArray[nums.length -1];
+    }
+
+    class Solution {
+        public int rob(int[] nums) {
+            int n = nums.length;
+            if (n == 1){
+                return nums[0];
+            }
+            int[] dp = new int[n];
+            dp[0] = nums[0];
+            dp[1] = Math.max(nums[0], nums[1]);
+            for (int i = 2; i < n; i++){
+                dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+            }
+            return dp[n - 1];
+        }
     }
 }
