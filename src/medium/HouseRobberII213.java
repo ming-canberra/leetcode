df20081f890fe8c15e3bf24047a6e24e1463b07a
Ming <ming.edentiti@gmail.com>
2021-07-10 22:23:17 +1000

    not bug free, referred to the hint

--- src/medium/HouseRobberII213.java
+++ src/medium/HouseRobberII213.java
@@ -1,12 +1,8 @@
 package medium;
 
-public class HouseRobberII213 {
-    public static void main(String[] args) {
-        HouseRobberII213 thisClass = new HouseRobberII213();
+import java.util.*;
 
-        int res = thisClass.rob(new int[]{2,7,9,3,1});
-        System.out.println(res);
-    }
+public class HouseRobberII213 {
     public int rob(int[] nums) {
         int [] helperArray;
 
@@ -43,6 +39,39 @@ public class HouseRobberII213 {
             }
             int tempResult2 = helperArray[nums.length -1];
             return Math.max(tempResult2, tempResult1);
+        }
+    }
+
+    class Solution {
+        public int rob(int[] nums) {
+            int n = nums.length;
+            if (n == 1){
+                return nums[0];
+            }
+            int[] dp = new int[n];
+            dp[0] = nums[0];
+            for (int i = 1; i < n - 1; i++){
+                if (i == 1){
+                    dp[i] = Math.max(nums[0], nums[1]);
+                }
+                else{
+                    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+                }
+            }
+            int dpNMinus2 = dp[n - 2];
+            Arrays.fill(dp, 0);
+            dp[1] = nums[1];
+            for (int i = 2; i < n; i++){
+                if (i == 2){
+                    dp[i] = Math.max(nums[1], nums[2]);
+                }
+                else{
+                    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+                }
+            }
+
+            int dpNMinus1 = dp[n - 1];
+            return Math.max(dpNMinus2, dpNMinus1);
         }
     }
 }
