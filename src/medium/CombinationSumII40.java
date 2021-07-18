b02d9f67feedc247a5ae8101ccb0639ae85ff669
Ming <ming.edentiti@gmail.com>
2021-07-18 11:40:18 +1000

    not bug free

---
+++ src/medium/CombinationSumII40.java
@@ -0,0 +1,35 @@
+package medium;
+
+import java.util.*;
+
+public class CombinationSumII40 {
+    class Solution {
+        List<List<Integer>> result;
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            result = new ArrayList<>();
+            Arrays.sort(candidates);
+            dfs(candidates, target, new ArrayList<>(), 0, 0, new boolean[candidates.length]);
+            return result;
+        }
+        private void dfs(int[] candidates, int target, List<Integer> list, int index, int sum, boolean[] used) {
+            if(sum == target){
+                result.add(new ArrayList<>(list));
+            }
+            else if (sum > target){
+                return;
+            }
+            else{
+                for (int i = index; i < candidates.length; i++){
+                    if (i > 0 && candidates[i] == candidates[i - 1] && !used[i - 1]){
+                        continue;
+                    }
+                    list.add(candidates[i]);
+                    used[i] = true;
+                    dfs(candidates, target, list, i + 1, sum + candidates[i], used);
+                    used[i] = false;
+                    list.remove(list.size() - 1);
+                }
+            }
+        }
+    }
+}
