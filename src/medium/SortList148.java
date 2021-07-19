cfa45d81e5bda61f777ac0af0a2a9b51ef4c6d44
Ming <ming.edentiti@gmail.com>
2021-07-19 17:29:17 +1000

    not bug free. need to do merge sort again

--- src/medium/SortList148.java
+++ src/medium/SortList148.java
@@ -48,4 +48,64 @@ public class SortList148 {
             return dummy.next;
         }
     }
+
+
+    class Solution2 {
+        public ListNode sortList(ListNode head) {
+            if (head == null || head.next == null){
+                return head;
+            }
+            ListNode second = getSecondHalfHead(head);// second could be shorter
+            ListNode sortedHead = sortList(head);
+            ListNode sortedSecond = sortList(second);
+            return merge(sortedHead, sortedSecond);
+        }
+
+        private ListNode getSecondHalfHead(ListNode head){
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            ListNode result = slow.next;
+            slow.next = null;
+            return result;
+        }
+
+        private ListNode merge(ListNode h1, ListNode h2){
+            if (h1 == null){
+                return h2;
+            }
+            if (h2 == null){
+                return h1;
+            }
+            ListNode cur1 = h1;
+            ListNode cur2 = h2;
+            ListNode dummyHead = new ListNode(0);
+            ListNode tail = dummyHead;
+            while(cur1 != null && cur2 != null){
+                ListNode tmp;
+                if (cur1.val < cur2.val){
+                    tmp = cur1;
+                    cur1 = cur1.next;
+                }
+                else{
+                    tmp = cur2;
+                    cur2 = cur2.next;
+                }
+                tmp.next = null;
+                tail.next = tmp;
+                tail = tmp;
+            }
+            if (cur1 != null){
+                tail.next = cur1;
+            }
+            if (cur2 != null){
+                tail.next = cur2;
+            }
+            return dummyHead.next;
+        }
+    }
 }
+
