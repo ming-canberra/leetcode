a0e8101449477828758472282299f2cf9602b412
Ming <ming.edentiti@gmail.com>
2021-07-24 11:25:35 +1000

    not bug free

--- src/easy/PathSum.java
+++ src/easy/PathSum.java
@@ -29,4 +29,35 @@ public class PathSum {
             return sum(current.left, ancestorAndMeSum, targetSum);
         }
     }
+
+    class Solution {
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if (root == null){
+                return false;
+            }
+            return foundPath(root, targetSum, 0);
+        }
+        private boolean foundPath(TreeNode root, int target, int sum){
+            if (root.left == null && root.right == null)
+            {
+                return sum + root.val == target;
+            }
+
+            if (root.left != null){
+                boolean l = foundPath(root.left, target, sum + root.val);
+                if (l){
+                    return true;
+                }
+            }
+
+            if (root.right != null){
+                boolean r = foundPath(root.right, target, sum + root.val);
+                if (r){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
 }
