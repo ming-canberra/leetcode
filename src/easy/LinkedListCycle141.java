5ffd1b2814e77471a708e48d2ad519490b2ce7b1
Ming <ming.edentiti@gmail.com>
2021-07-28 00:07:39 +1000

    bug free, nailed it

--- src/easy/LinkedListCycle141.java
+++ src/easy/LinkedListCycle141.java
@@ -26,4 +26,22 @@ public class LinkedListCycle141 {
 
         return true;
     }
+
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            if (head == null){
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while(fast != null && fast.next != null){
+                if (slow == fast){
+                    return true;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return false;
+        }
+    }
 }
