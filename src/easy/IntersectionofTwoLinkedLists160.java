0c44bd8f836eecc2584cb6cf8fb2a16f2e5e20c1
Ming <ming.edentiti@gmail.com>
2021-07-28 17:26:14 +1000

    bug free, optimal, referred to solution, need to do it again

--- src/easy/IntersectionofTwoLinkedLists160.java
+++ src/easy/IntersectionofTwoLinkedLists160.java
@@ -65,4 +65,29 @@ public class IntersectionofTwoLinkedLists160 {
             return count;
         }
     }
+
+    public class SolutionTricky {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null || headB == null){
+                return null;
+            }
+            ListNode nodeA = headA;
+            ListNode nodeB = headB;
+            while(nodeA != nodeB){
+                if(nodeA == null){
+                    nodeA = headB;
+                }
+                else{
+                    nodeA = nodeA.next;
+                }
+                if(nodeB == null){
+                    nodeB = headA;
+                }
+                else{
+                    nodeB = nodeB.next;
+                }
+            }
+            return nodeA;
+        }
+    }
 }
