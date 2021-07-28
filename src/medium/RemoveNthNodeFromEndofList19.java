b777f5e3b758fddcd26fc6198f7e8bbf9705606d
Ming <ming.edentiti@gmail.com>
2021-07-28 21:27:15 +1000

    bug free, nailed it

--- src/medium/RemoveNthNodeFromEndofList19.java
+++ src/medium/RemoveNthNodeFromEndofList19.java
@@ -28,4 +28,22 @@ public class RemoveNthNodeFromEndofList19 {
 
         return head;
     }
+
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(1);
+            dummy.next = head;
+            ListNode left = dummy;
+            ListNode right = dummy;
+            for (int i = 0; i < n; i++){
+                right = right.next;
+            }
+            while(right.next != null){
+                right = right.next;
+                left = left.next;
+            }
+            left.next = left.next.next;
+            return dummy.next;
+        }
+    }
 }
