34cfa8dd11d7a42c33bf705fc603761e1ad0c184
Ming <ming.edentiti@gmail.com>
2021-07-28 12:55:37 +1000

    not optimal, 1 bug.

--- src/easy/IntersectionofTwoLinkedLists160.java
+++ src/easy/IntersectionofTwoLinkedLists160.java
@@ -24,5 +24,45 @@ public class IntersectionofTwoLinkedLists160 {
             return null;
         }
     }
-
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null || headB == null){
+                return null;
+            }
+            int lengthA = getLength(headA);
+            int lengthB = getLength(headB);
+            ListNode shortCurNode;
+            ListNode longCurNode;
+            if (lengthA < lengthB){
+                shortCurNode = headA;
+                longCurNode = headB;
+            }
+            else{
+                shortCurNode = headB;
+                longCurNode = headA;
+            }
+            int diff = Math.abs(lengthA - lengthB);
+            for (int i = 0; i < diff; i++){
+                longCurNode = longCurNode.next;
+            }
+            while(shortCurNode != null){
+                if (shortCurNode == longCurNode){
+                    return longCurNode;
+                }
+                else{
+                    shortCurNode = shortCurNode.next;
+                    longCurNode = longCurNode.next;
+                }
+            }
+            return null;
+        }
+        private int getLength(ListNode head){
+            int count = 0;
+            while(head != null){
+                count++;
+                head = head.next;
+            }
+            return count;
+        }
+    }
 }
