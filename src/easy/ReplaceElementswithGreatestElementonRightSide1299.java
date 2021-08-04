86d615338570160e7dee95dca8b3026f3a47dbae
Ming <ming.edentiti@gmail.com>
2021-08-04 23:04:24 +1000

    bug free, nailed it

---
+++ src/easy/ReplaceElementswithGreatestElementonRightSide1299.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class ReplaceElementswithGreatestElementonRightSide1299 {
+    class Solution {
+        public int[] replaceElements(int[] arr) {
+            int n = arr.length;
+            int max = arr[n - 1];
+            arr[n - 1] = -1;
+            for (int i = n - 2; i >= 0; i--){
+                int tmp = arr[i];
+                arr[i] = max;
+                max = Math.max(max, tmp);
+            }
+            return arr;
+        }
+    }
+}
