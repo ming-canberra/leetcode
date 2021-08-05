b248fdc29dc80d1abef04a325b3bc335cf5a156f
Ming <ming.edentiti@gmail.com>
2021-08-05 16:32:53 +1000

    bug free, nailed it

--- src/easy/RemoveElement27.java
+++ src/easy/RemoveElement27.java
@@ -32,4 +32,22 @@ public class RemoveElement27 {
             nums[b] = tmp;
         }
     }
+
+    class Solution1 {
+        public int removeElement(int[] nums, int val) {
+            int cur = -1;
+            for (int i = 0; i < nums.length; i++){
+                if (nums[i] != val){
+                    cur++;
+                    swap(nums, cur, i);
+                }
+            }
+            return cur + 1;
+        }
+        private void swap(int[] nums, int a, int b){
+            int tmp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = tmp;
+        }
+    }
 }
