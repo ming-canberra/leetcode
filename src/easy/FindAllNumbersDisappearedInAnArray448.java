2135bc37e5ad6e9e7f57619a6d8c4de95d6a3396
Ming <ming.edentiti@gmail.com>
2021-08-06 00:28:57 +1000

    bug free, nailed it

--- src/easy/FindAllNumbersDisappearedInAnArray448.java
+++ src/easy/FindAllNumbersDisappearedInAnArray448.java
@@ -21,4 +21,22 @@ public class FindAllNumbersDisappearedInAnArray448 {
             return result;
         }
     }
+
+    class SolutionOptimal {
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            for (int i = 0; i < nums.length; i++){
+                int index = Math.abs(nums[i]) - 1;
+                if (nums[index] > 0){
+                    nums[index] = -nums[index];
+                }
+            }
+            List<Integer> result = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++){
+                if (nums[i] > 0){
+                    result.add(i + 1);
+                }
+            }
+            return result;
+        }
+    }
 }
