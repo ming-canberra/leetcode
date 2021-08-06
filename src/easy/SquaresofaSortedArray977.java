6f5dcd30644ec7b615192a26d1517aa6c792b836
Ming <ming.edentiti@gmail.com>
2021-08-06 11:13:41 +1000

    bug free, 1 typo, nailed it

--- src/easy/SquaresofaSortedArray977.java
+++ src/easy/SquaresofaSortedArray977.java
@@ -40,4 +40,24 @@ public class SquaresofaSortedArray977 {
             return result;
         }
     }
+
+    class SolutionLogN {
+        public int[] sortedSquares(int[] nums) {
+            int n = nums.length;
+            int[] result = new int[n];
+            int l = 0;
+            int r = n - 1;
+            for (int i = n - 1; i >= 0; i--){
+                if (Math.abs(nums[l]) > Math.abs(nums[r])){
+                    result[i] = nums[l] * nums[l];
+                    l++;
+                }
+                else{
+                    result[i] = nums[r] * nums[r];
+                    r--;
+                }
+            }
+            return result;
+        }
+    }
 }
