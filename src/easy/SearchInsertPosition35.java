2baeb7abe20e429d59f5408268fafe83374b0e63
Ming <ming.edentiti@gmail.com>
2021-08-08 16:29:16 +1000

    need to clarify the requirement, otherwise, bug free

--- src/easy/SearchInsertPosition35.java
+++ src/easy/SearchInsertPosition35.java
@@ -31,4 +31,24 @@ public class SearchInsertPosition35 {
             return l;
         }
     }
+
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int l = -1;
+            int r = nums.length;
+            while(l + 1 < r){
+                int m = l + (r - l) / 2;
+                if (nums[m] == target){
+                    return m;
+                }
+                else if (nums[m] > target){
+                    r = m;
+                }
+                else{
+                    l = m;
+                }
+            }
+            return r;
+        }
+    }
 }
