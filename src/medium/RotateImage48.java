9fb28057314b9b4154c98f9091126467d6772b84
Ming <ming.edentiti@gmail.com>
2021-08-13 00:06:52 +1000

    nailed it, bug free

--- src/medium/RotateImage48.java
+++ src/medium/RotateImage48.java
@@ -15,4 +15,24 @@ public class RotateImage48 {
             }
         }
     }
+
+    class Solution1 {
+        public void rotate(int[][] matrix) {
+            int n = matrix.length;
+            for (int i = 0; i < n / 2; i++){
+                for (int j = i; j < n - i - 1; j++){
+                    // top
+                    int tmp = matrix[i][j];
+                    // left to top
+                    matrix[i][j] = matrix[n - 1 - j][i];
+                    // bottom to left
+                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                    // right to bottom
+                    matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                    // top to right
+                    matrix[j][n - 1 - i] = tmp;
+                }
+            }
+        }
+    }
 }
