68022a0f38259b86264897a50b827a0c8c339485
Ming <ming.edentiti@gmail.com>
2021-08-12 21:52:34 +1000

    nailed it, bug free

--- src/medium/PermutationsII47.java
+++ src/medium/PermutationsII47.java
@@ -31,4 +31,33 @@ public class PermutationsII47 {
             }
         }
     }
+
+    class Solution1 {
+        List<List<Integer>> result = new ArrayList<>();
+        public List<List<Integer>> permuteUnique(int[] nums) {
+            Arrays.sort(nums);
+            boolean[] used = new boolean[nums.length];
+            dfs(nums, used, new ArrayList<>());
+            return result;
+        }
+        private void dfs(int[] nums, boolean[] used, List<Integer> list){
+            if (list.size() == nums.length){
+                result.add(new ArrayList<>(list));
+            }
+            else{
+                for (int i = 0; i < nums.length; i++){
+                    if (i != 0 && nums[i - 1] == nums[i] && !used[i - 1]){
+                        continue;
+                    }
+                    if (!used[i]){
+                        used[i] = true;
+                        list.add(nums[i]);
+                        dfs(nums, used, list);
+                        list.remove(list.size() - 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+    }
 }
