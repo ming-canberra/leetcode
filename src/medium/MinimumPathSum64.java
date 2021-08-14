4b38f5874636e81d041f7564c456ab0b8cccf8ce
Ming <ming.edentiti@gmail.com>
2021-08-14 16:23:36 +1000

    bug free, nailed it within 20 mins

--- src/medium/MinimumPathSum64.java
+++ src/medium/MinimumPathSum64.java
@@ -1,12 +1,6 @@
 package medium;
 
 public class MinimumPathSum64 {
-    public static void main(String[] args) {
-        MinimumPathSum64 thisClass = new MinimumPathSum64();
-
-        int res = thisClass.minPathSum(new int[][]{new int[]{1,2,3}, new int[]{4,5,6}});
-        System.out.println(res);
-    }
     public int minPathSum(int[][] grid) {
         int[][] dp = new int[grid.length][grid[0].length];
         for (int i = 0; i < grid.length;i++){
@@ -29,5 +23,24 @@ public class MinimumPathSum64 {
         }
 
         return dp[dp.length - 1][dp[0].length - 1];
+    }
+
+    public int minPathSum1(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++){
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++){
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
