5c07312f68adad587705a43121311fe6ace083c8
Ming <ming.edentiti@gmail.com>
2021-08-14 23:09:08 +1000

    bug free, nailed it within 20 mins

--- src/medium/Combinations77.java
+++ src/medium/Combinations77.java
@@ -22,4 +22,24 @@ public class Combinations77 {
             }
         }
     }
+
+    class Solution1 {
+        List<List<Integer>> result = new ArrayList<>();
+        public List<List<Integer>> combine(int n, int k) {
+            dfs(n, k, 1, new ArrayList<>());
+            return result;
+        }
+        private void dfs(int n, int k, int num, List<Integer> list){
+            if (list.size() == k){
+                result.add(new ArrayList<>(list));
+            }
+            else{
+                for (int i = num; i <= n; i++){
+                    list.add(i);
+                    dfs(n, k, i + 1, list);
+                    list.remove(list.size() - 1);
+                }
+            }
+        }
+    }
 }
