f0eae497a05767dad223d3011578e85d2919a4c2
Ming <ming.edentiti@gmail.com>
2021-08-18 23:46:04 +1000

    bug free, nailed it within 45 mins.

--- src/medium/HouseRobberII213.java
+++ src/medium/HouseRobberII213.java
@@ -74,4 +74,35 @@ public class HouseRobberII213 {
             return Math.max(dpNMinus2, dpNMinus1);
         }
     }
+
+    class Solution1 {
+        public int rob(int[] nums) {
+            int n = nums.length;
+            if (n == 1){
+                return nums[0];
+            }
+            int result = 0;
+            int[] dp = new int[n];
+            //rob the first one
+            dp[0] = nums[0];
+            dp[1] = nums[0];
+            for (int i = 2; i < n; i++){
+                if (i == n - 1){
+                    dp[i] = Math.max(dp[i - 1], dp[i - 2]);
+                }
+                else{
+                    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+                }
+            }
+            result = Math.max(result, dp[n - 1]);
+            //not rob the first one
+            dp[0] = 0;
+            dp[1] = nums[1];
+            for (int i = 2; i < n; i++){
+                dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+            }
+            result = Math.max(result, dp[n - 1]);
+            return result;
+        }
+    }
 }
