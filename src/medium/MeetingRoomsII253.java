c3a0cb2a8502ee6df1790e5a9519ccd9fb4b2a47
Ming <ming.edentiti@gmail.com>
2021-08-19 18:40:22 +1000

    bug free, nailed it within 13 mins.

--- src/medium/MeetingRoomsII253.java
+++ src/medium/MeetingRoomsII253.java
@@ -110,4 +110,20 @@ public class MeetingRoomsII253 {
         }
         return heap.size();
     }
+
+    class Solution1 {
+        public int minMeetingRooms(int[][] intervals) {
+            Arrays.sort(intervals, (a, b)->a[0] - b[0]);
+            PriorityQueue<int[]> heap = new PriorityQueue<>( (a, b)-> a[1] - b[1] );
+            for (int i = 0; i < intervals.length; i++){
+                if (!heap.isEmpty()){
+                    if (heap.peek()[1] <= intervals[i][0]){
+                        heap.poll();
+                    }
+                }
+                heap.add(intervals[i]);
+            }
+            return heap.size();
+        }
+    }
 }
