701cf2daa44fa77b585395dc8e3074c3fbce7eeb
Ming <ming.edentiti@gmail.com>
2021-08-20 00:49:31 +1000

    1 typo bug, nailed it within 35 mins. Do it again since is it a classic DP

--- src/medium/LongestIncreasingSubsequence300.java
+++ src/medium/LongestIncreasingSubsequence300.java
@@ -1,19 +1,24 @@
 package medium;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class LongestIncreasingSubsequence300 {
-    public static void main(String[] args) {
-        LongestIncreasingSubsequence300 thisClass = new LongestIncreasingSubsequence300();
-
-        int res = thisClass.lengthOfLIS(new int[]{10,9,2,5,3,7,101,18});
-        System.out.println(res);
-    }
-
-    public int lengthOfLIS(int[] nums) {
-
-
-
-        return 234;
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            int n = nums.length;
+            int[] dp = new int[n]; // ith element, length of LIS ending with element i
+            int result = 1;
+            for (int i = 0; i < n; i++){
+                int max = 1;
+                for (int j = 0; j < i; j++){
+                    if (nums[i] > nums[j]){
+                        max = Math.max(max, dp[j] + 1);
+                    }
+                }
+                dp[i] = max;
+                result = Math.max(result, dp[i]);
+            }
+            return result;
+        }
     }
 }
