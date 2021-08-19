0b0b547245f42ddc9c55ddafbd99675315786b47
Ming <ming.edentiti@gmail.com>
2021-08-19 12:03:23 +1000

    1 typo bug, nailed it within 35 mins.

--- src/medium/MaximalSquare221.java
+++ src/medium/MaximalSquare221.java
@@ -46,4 +46,23 @@ public class MaximalSquare221 {
 
         return result * result;
     }
+
+    class Solution {
+        public int maximalSquare(char[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int[][] dp = new int[m + 1][n + 1];
+            int result = 0;
+            for (int i = 0; i < m; i++){
+                for (int j = 0; j < n; j++){
+                    if (matrix[i][j] == '1'){
+                        int min = Math.min(dp[i][j + 1], Math.min(dp[i][j], dp[i + 1][j]));
+                        dp[i + 1][j + 1] = min + 1;
+                        result = Math.max(result, min + 1);
+                    }
+                }
+            }
+            return result * result;
+        }
+    }
 }
