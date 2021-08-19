bd1a516e3d7e9fb73b1a8e1177c259535b60f86f
Ming <ming.edentiti@gmail.com>
2021-08-19 16:23:39 +1000

    bug free, nailed it within 20 mins.

--- src/medium/ProductOfArrayExceptSelf238.java
+++ src/medium/ProductOfArrayExceptSelf238.java
@@ -30,4 +30,29 @@ public class ProductOfArrayExceptSelf238 {
 
         return result;
     }
+
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int n = nums.length;
+            int[] prePro = new int[n];
+            int[] sufPro = new int[n];
+            int product = 1;
+            for (int i = 0; i < n - 1; i++){
+                product = product * nums[i];
+                prePro[i] = product;
+            }
+            product = 1;
+            for (int i = n - 1; i > 0; i--){
+                product = product * nums[i];
+                sufPro[i] = product;
+            }
+            int[] result = new int[n];
+            result[0] = sufPro[1];
+            result[n - 1] = prePro[n - 2];
+            for (int i = 1; i < n - 1; i++){
+                result[i] = prePro[i - 1] * sufPro[i + 1];
+            }
+            return result;
+        }
+    }
 }
