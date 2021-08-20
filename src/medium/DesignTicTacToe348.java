39bdb93bfaaf84de6d5b7c79e371edc76f35d526
Ming <ming.edentiti@gmail.com>
2021-08-20 23:48:44 +1000

    2 typo bugs, nailed it within 23 mins

--- src/medium/DesignTicTacToe348.java
+++ src/medium/DesignTicTacToe348.java
@@ -45,9 +45,57 @@ public class DesignTicTacToe348 {
 
             return 0;
         }
+    }
 
+    class TicTacToe1 {
+        private int length;
+        private int[] rowSum;
+        private int[] colSum;
+        private int slashSum = 0 ;
+        private int backSlashSum = 0;
 
+        public TicTacToe1(int n) {
+            rowSum = new int[n];
+            colSum = new int[n];
+            length = n;
+        }
 
-
+        /** Player {player} makes a move at ({row}, {col}).
+         @param row The row of the board.
+         @param col The column of the board.
+         @param player The player, can be either 1 or 2.
+         @return The current winning condition, can be either:
+         0: No one wins.
+         1: Player 1 wins.
+         2: Player 2 wins. */
+        public int move(int row, int col, int player) {
+            if (player == 1){
+                rowSum[row]++;
+                colSum[col]++;
+                if (row == col){
+                    backSlashSum++;
+                }
+                if (row + col == length - 1){
+                    slashSum++;
+                }
+                if (rowSum[row] == length || colSum[col] == length || backSlashSum == length || slashSum == length){
+                    return 1;
+                }
+            }
+            else{
+                rowSum[row]--;
+                colSum[col]--;
+                if (row == col){
+                    backSlashSum--;
+                }
+                if (row + col == length - 1){
+                    slashSum--;
+                }
+                if (rowSum[row] == -length || colSum[col] == -length || backSlashSum == -length || slashSum == -length){
+                    return 2;
+                }
+            }
+            return 0;
+        }
     }
 }
