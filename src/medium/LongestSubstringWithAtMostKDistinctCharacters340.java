546f29c653d6c9c6e84c7ac16f3d54198ab9280d
Ming <ming.edentiti@gmail.com>
2021-08-31 17:12:06 +1000

    bug free, nailed it within 20 mins. I have done it so many times, and it has become a template

--- src/medium/LongestSubstringWithAtMostKDistinctCharacters340.java
+++ src/medium/LongestSubstringWithAtMostKDistinctCharacters340.java
@@ -30,4 +30,34 @@ public class LongestSubstringWithAtMostKDistinctCharacters340 {
             return result;
         }
     }
+
+    class Solution1 {
+        public int lengthOfLongestSubstringKDistinct(String s, int k) {
+            if (k == 0){
+                return 0;
+            }
+            int result = 0;
+            Map<Character, Integer> map = new HashMap<>();// char to count
+            int left = 0;
+            for (int i = 0; i < s.length(); i++){
+                Character c = s.charAt(i);
+                if (map.containsKey(c)){
+                    map.put(c, map.get(c) + 1);
+                }
+                else{
+                    if (map.size() == k){
+                        while(map.get(s.charAt(left)) != 1 ){
+                            map.put(s.charAt(left),  map.get(s.charAt(left)) - 1);
+                            left++;
+                        }
+                        map.remove(s.charAt(left));
+                        left++;
+                    }
+                    map.put(c, 1);
+                }
+                result = Math.max(result, i - left + 1);
+            }
+            return result;
+        }
+    }
 }
