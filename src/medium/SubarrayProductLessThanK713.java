1101aece4ce557b88b2cefac8f2717eda0947316
Ming <ming.edentiti@gmail.com>
2021-09-02 00:13:59 +1000

    overtime, did not count the result correctly. Do it again

--- src/medium/SubarrayProductLessThanK713.java
+++ src/medium/SubarrayProductLessThanK713.java
@@ -22,4 +22,24 @@ public class SubarrayProductLessThanK713 {
             return result;
         }
     }
+
+    class Solution1 {
+        public int numSubarrayProductLessThanK(int[] nums, int k) {
+            if (k == 0 || k == 1){
+                return 0;
+            }
+            int result = 0;
+            int left = 0;
+            int prod = 1;
+            for (int right = 0; right < nums.length; right++){
+                prod = prod * nums[right];
+                while(prod >= k){
+                    prod = prod / nums[left];
+                    left++;
+                }
+                result += right - left + 1;
+            }
+            return result;
+        }
+    }
 }
