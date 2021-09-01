64721f6a575953ceed62ac84f2798f63dd5fddf7
Ming <ming.edentiti@gmail.com>
2021-09-01 23:17:32 +1000

    1 typo bug, nailed it within 13 mins

--- src/medium/PermutationinString567.java
+++ src/medium/PermutationinString567.java
@@ -32,4 +32,29 @@ public class PermutationinString567 {
             return false;
         }
     }
+
+    class Solution1 {
+        public boolean checkInclusion(String s1, String s2) {
+            if (s1.length() > s2.length()){
+                return false;
+            }
+            int[] sMap = new int[26];
+            int[] bMap = new int[26];
+            for (int i = 0; i < s1.length(); i++){
+                sMap[s1.charAt(i) - 'a']++;
+                bMap[s2.charAt(i) - 'a']++;
+            }
+            if(Arrays.equals(sMap, bMap)){
+                return true;
+            }
+            for (int i = s1.length(); i < s2.length(); i++){
+                bMap[s2.charAt(i) - 'a']++;
+                bMap[s2.charAt(i - s1.length()) - 'a']--;
+                if(Arrays.equals(sMap, bMap)){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
