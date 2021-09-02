931fb79dcbac42c7ac5e16d1de0b38f7696fcdd7
Ming <ming.edentiti@gmail.com>
2021-09-03 00:57:31 +1000

    bug free, nailed it within 43 mins

--- src/medium/ContinuousSubarraySum523.java
+++ src/medium/ContinuousSubarraySum523.java
@@ -28,4 +28,26 @@ public class ContinuousSubarraySum523 {
             return false;
         }
     }
+
+    class Solution1 {
+        public boolean checkSubarraySum(int[] nums, int k) {
+            int len = nums.length;
+            int sum = nums[0] % k;
+            Set<Integer> set = new HashSet<>();
+            int prSum = 0;
+            for (int i = 1; i < len; i++){
+                prSum = sum;
+                sum += nums[i];
+                sum = sum % k;
+                if (sum == 0){
+                    return true;
+                }
+                if (set.contains(sum) ){
+                    return true;
+                }
+                set.add(prSum);
+            }
+            return false;
+        }
+    }
 }
