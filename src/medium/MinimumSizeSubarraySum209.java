ebc9d1617e8e504906bfacc7dac38f5cefe4e9ce
Ming <ming.edentiti@gmail.com>
2021-09-02 19:01:32 +1000

    bug free, nailed it within 20 mins

--- src/medium/MinimumSizeSubarraySum209.java
+++ src/medium/MinimumSizeSubarraySum209.java
@@ -69,4 +69,21 @@ public class MinimumSizeSubarraySum209 {
             return result == null ? 0 : result;
         }
     }
+
+    class Solution1 {
+        public int minSubArrayLen(int target, int[] nums) {
+            int result = Integer.MAX_VALUE;
+            int left = 0;
+            int sum = 0;
+            for (int i = 0; i < nums.length; i++){
+                sum += nums[i];
+                while(sum >= target){
+                    result = Math.min(result, i - left + 1);
+                    sum -= nums[left];
+                    left++;
+                }
+            }
+            return result == Integer.MAX_VALUE ? 0 : result;
+        }
+    }
 }
