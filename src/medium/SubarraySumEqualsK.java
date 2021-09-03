3e4828bac6803e561fc1a80e3e50735af5e7914d
Ming <ming.edentiti@gmail.com>
2021-09-03 11:44:55 +1000

    bug free, nailed it within 10 mins

--- src/medium/SubarraySumEqualsK.java
+++ src/medium/SubarraySumEqualsK.java
@@ -57,4 +57,23 @@ public class SubarraySumEqualsK {
         }
         return result;
     }
+
+    class Solution {
+        public int subarraySum(int[] nums, int k) {
+            int sum = 0;
+            int result = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++){
+                sum += nums[i];
+                if (sum == k){
+                    result++;
+                }
+                if (map.containsKey(sum - k)){
+                    result += map.get(sum -k);
+                }
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+            return result;
+        }
+    }
 }
