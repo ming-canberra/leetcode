a7bac530fac938b174dc2eec42790f13b0334c2e
Ming <ming.edentiti@gmail.com>
2021-09-04 14:46:24 +1000

    referred to solution. Do this again

---
+++ src/medium/Medium134GasStation.java
@@ -0,0 +1,32 @@
+package medium;
+
+public class Medium134GasStation {
+    class Solution {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int n = gas.length;
+            int gSum = 0;
+            int cSum = 0;
+            for (int i = 0; i < n; i++){
+                gSum += gas[i];
+                cSum += cost[i];
+            }
+            if (gSum >= cSum){
+                int tank = 0;
+                int result = 0;
+                for (int i = 0; i < n; i++){
+                    tank += gas[i];
+                    if (tank >= cost[i]){
+                        tank -= cost[i];
+                        continue;
+                    }
+                    else{
+                        tank = 0;
+                        result = i + 1;
+                    }
+                }
+                return result;
+            }
+            return -1;
+        }
+    }
+}
