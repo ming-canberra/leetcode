06876f84fcbf96498c376dffd249b8970e598f70
Ming <ming.edentiti@gmail.com>
2021-09-04 23:05:38 +1000

    space optimal, within time, do this again

--- src/medium/Medium189RotateArray.java
+++ src/medium/Medium189RotateArray.java
@@ -15,4 +15,33 @@ public class Medium189RotateArray {
             }
         }
     }
+
+    class Solution1 {
+        public void rotate(int[] nums, int k) {
+            int len = nums.length;
+            k = k % len;
+            if (k ==0){
+                return;
+            }
+            int startIdx = 0;
+            int lastValue = nums[0];
+            int curIdx = 0;
+            int nextIdx = 0;
+            for (int i = 0; i < len; i++){
+                nextIdx = curIdx + k >= len ? curIdx + k - len : curIdx + k;
+                int tmp = nums[nextIdx];
+                nums[nextIdx] = lastValue;
+                lastValue = tmp;
+                curIdx = nextIdx;
+                if (curIdx == startIdx){
+                    startIdx++;
+                    curIdx = startIdx;
+                    if (curIdx < len)
+                    {
+                        lastValue = nums[curIdx];
+                    }
+                }
+            }
+        }
+    }
 }
