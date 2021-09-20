59a0d7aaf3e09ebe271de6a2ef92a62e62bdba30
Ming <ming.edentiti@gmail.com>
2021-09-21 02:19:11 +1000

    do this another 2 times, 0 1 knapsack problem

--- src/medium/Medium474OnesandZeroes.java
+++ src/medium/Medium474OnesandZeroes.java
@@ -31,4 +31,35 @@ public class Medium474OnesandZeroes {
             return dp[len][m][n];
         }
     }
+
+    class Solution1 {
+        public int findMaxForm(String[] strs, int m, int n) {
+            int[][][] dp = new int[strs.length + 1][m + 1][n + 1];
+            for (int i = 1; i < strs.length + 1; i++){
+                int one = 0;
+                int zero = 0;
+                for (char c : strs[i - 1].toCharArray()){
+                    if (c == '0'){
+                        zero++;
+                    }
+                    else{
+                        one++;
+                    }
+                }
+                for (int j = 0; j < m + 1; j++){
+                    for (int k = 0; k < n + 1; k++){
+
+                        // select it
+                        int value1 = 0;
+                        if (j >= zero && k >= one){
+                            value1 = 1 + dp[i - 1][j - zero][k - one];
+                        }
+                        // not select it
+                        dp[i][j][k] = Math.max(value1, dp[i - 1][j][k]);
+                    }
+                }
+            }
+            return dp[strs.length][m][n];
+        }
+    }
 }
