e42bcccac54ecd9edeca848231ac35ed474456fd
Ming <ming.edentiti@gmail.com>
2021-09-22 23:29:26 +1000

    do this again at least 2 times, variant of the longest increasing subsequence

--- src/medium/Medium1626BestTeamWithNoConflicts.java
+++ src/medium/Medium1626BestTeamWithNoConflicts.java
@@ -30,4 +30,27 @@ public class Medium1626BestTeamWithNoConflicts {
             return result;
         }
     }
+
+    class Solution1 {
+        public int bestTeamScore(int[] scores, int[] ages) {
+            int len = ages.length;
+            int[][] arr = new int[len][2];
+            for (int i = 0; i < len; i++){
+                arr[i] = new int[]{scores[i], ages[i]};
+            }
+            Arrays.sort(arr, (a, b)-> a[1] == b[1] ? b[0] - a[0] :  b[1] - a[1]);
+            int[]dp = new int[len];
+            int result = arr[0][0];
+            for (int i = 0; i < len; i++){
+                dp[i] = arr[i][0];
+                for (int j = 0; j < i; j++){
+                    if (arr[i][0] <= arr[j][0]){
+                        dp[i] = Math.max(dp[i], dp[j] + arr[i][0]);
+                    }
+                }
+                result = Math.max(result, dp[i]);
+            }
+            return result;
+        }
+    }
 }
