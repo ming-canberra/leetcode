22d3a7e4d5c65138cc1feb3751333fa520247a38
Ming <ming.edentiti@gmail.com>
2021-09-23 14:21:48 +1000

    do this again at least another 3 times, pay attention to long int conversion and module.

--- src/medium/Medium1191KConcatenationMaximumSum.java
+++ src/medium/Medium1191KConcatenationMaximumSum.java
@@ -2,31 +2,40 @@ package medium;
 
 public class Medium1191KConcatenationMaximumSum {
     class Solution {
-        public int canCompleteCircuit(int[] gas, int[] cost) {
-            int n = gas.length;
-            int gSum = 0;
-            int cSum = 0;
-            for (int i = 0; i < n; i++){
-                gSum += gas[i];
-                cSum += cost[i];
+        public int kConcatenationMaxSum(int[] arr, int k) {
+            int len = arr.length;
+            int[] arr2 = new int[len * 2];
+            int sum = 0;
+            for (int i = 0; i < len; i++){
+                if (i < len){
+                    sum += arr[i];
+                }
+                arr2[i] = arr[i];
+                arr2[i + len] = arr[i];
             }
-            if (gSum >= cSum){
-                int tank = 0;
-                int result = 0;
-                for (int i = 0; i < n; i++){
-                    tank += gas[i];
-                    if (tank >= cost[i]){
-                        tank -= cost[i];
-                        continue;
-                    }
-                    else{
-                        tank = 0;
-                        result = i + 1;
+            long[] dp = new long[len * 2];
+            long max1 = arr[0];
+            long max2 = arr[0];
+            dp[0] = arr[0];
+            for (int i = 1; i < dp.length; i++){
+                dp[i] = Math.max(arr2[i], (dp[i - 1] + arr2[i]));
+                if (i < len){
+                    max1 = Math.max(max1, dp[i]);
+                }
+                max2 = Math.max(max2, dp[i]);
+            }
+            if (k == 1){
+                return max1 < 0 ? 0 : (int)(max1 % 1000000007);
+            }
+            else{
+                long result = max2;
+                if (sum > 0){
+                    for (int i = k - 2; i > 0; i--){
+                        result = (result + sum) % 1000000007;
                     }
                 }
-                return result;
+                return result < 0 ? 0 : (int)(result % 1000000007);
             }
-            return -1;
         }
     }
 }
