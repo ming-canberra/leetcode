d8b9a0735d25cdf999c82b0b4c937054b25979e3
Ming <ming.edentiti@gmail.com>
2021-09-25 01:18:49 +1000

    1 typo bug, nailed it within 25 mins

--- src/medium/LongestPalindromicSubsequence516.java
+++ src/medium/LongestPalindromicSubsequence516.java
@@ -26,4 +26,25 @@ public class LongestPalindromicSubsequence516 {
             return dp[0][n - 1];
         }
     }
+
+    class Solution1 {
+        public int longestPalindromeSubseq(String s) {
+            StringBuilder sb = new StringBuilder(s);
+            sb.reverse();
+            String sr = sb.toString();
+            int len = s.length();
+            int[][] dp = new int[len + 1][len + 1];
+            int result = 1;
+            for (int i = 1; i < len + 1; i++){
+                for (int j = 1; j < len + 1; j++){
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    if (sr.charAt(i - 1) == s.charAt(j - 1)){
+                        dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+                    }
+                    result = Math.max(result, dp[i][j]);
+                }
+            }
+            return result;
+        }
+    }
 }
