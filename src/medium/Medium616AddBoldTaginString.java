5f494477dfae557d3dfbc6528d25380caef62e34
Ming <ming.edentiti@gmail.com>
2021-10-02 14:54:32 +1000

    overtime, do this again

--- src/medium/Medium616AddBoldTaginString.java
+++ src/medium/Medium616AddBoldTaginString.java
@@ -32,4 +32,39 @@ public class Medium616AddBoldTaginString {
             return sb.toString();
         }
     }
+
+    class Solution1 {
+        public String addBoldTag(String s, String[] words) {
+            StringBuilder sb = new StringBuilder();
+            Arrays.sort(words, (a, b)->a.length() - b.length());
+            for (int i = 0; i < s.length(); i++){
+                int startIndex = i;
+                int endIndex = -1;//
+                for (int j = 0; j < words.length; j++){
+                    if ( s.substring(i).startsWith(words[j]) ) {
+                        endIndex = i + words[j].length();
+                    }
+                }
+                while (endIndex > -1 && i < endIndex){
+                    i++;
+                    for (int j = 0; j < words.length; j++){
+                        if (s.substring(i).startsWith(words[j])){
+                            endIndex = Math.max(endIndex, i + words[j].length());
+                        }
+                    }
+                }
+                if (endIndex != -1){
+                    sb.append("<b>");
+                    sb.append(s.substring(startIndex, endIndex));
+                    sb.append("</b>");
+                    if (i < s.length())
+                        sb.append(s.charAt(i));
+                }
+                else{
+                    sb.append(s.charAt(i));
+                }
+            }
+            return sb.toString();
+        }
+    }
 }
