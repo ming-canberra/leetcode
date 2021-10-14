3bbc601501a72447a1bf1cd9c47c38f8b606a0f6
Ming <ming.edentiti@gmail.com>
2021-10-14 15:57:22 +1100

    harder than stone game 1, do this again.

--- src/medium/StoneGameII1140.java
+++ src/medium/StoneGameII1140.java
@@ -40,4 +40,35 @@ public class StoneGameII1140 {
             return memo[start][m];
         }
     }
+    class Solution1 {
+        public int stoneGameII(int[] piles) {
+            int len = piles.length;
+            int[][] memo = new int[len][len]; // start, m
+            int[] sum = new int[len + 1];
+            sum[0] = 0;
+            for (int i = 1; i < len + 1; i++){
+                sum[i] = sum[i - 1] + piles[i - 1];
+            }
+            int aliceDiff = getOptimal(piles, 1, 0, sum, memo);
+            return (aliceDiff + sum[len]) / 2;
+        }
+        // sum(i, j) = sum[j + 1] - sum[i]
+        private int getOptimal(int[] piles, int m, int start, int[] sum, int[][] memo){
+            if (m * 2 >= piles.length - start){
+                return sum[piles.length] - sum[start];
+            }
+            if (memo[start][m] != 0){
+                return memo[start][m];
+            }
+            int max = Integer.MIN_VALUE;
+            for (int i = start; i < start + m * 2; i++){
+                int tmpMax = sum[i + 1] - sum[start] - getOptimal(piles, Math.max(m, i - start + 1), i + 1, sum, memo);
+                if (tmpMax > max){
+                    max = tmpMax;
+                }
+            }
+            memo[start][m] = max;
+            return memo[start][m];
+        }
+    }
 }
