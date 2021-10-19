abe270abb9ae5777f167d27c2b6004fea42e8f69
Ming <ming.edentiti@gmail.com>
2021-10-19 20:12:39 +1100

    my second ever deque problem, do this again

--- src/medium/Medium1696JumpGame.java
+++ src/medium/Medium1696JumpGame.java
@@ -20,4 +20,29 @@ public class Medium1696JumpGame {
             return dp[0];
         }
     }
+
+    class Solution1 {
+        public int maxResult(int[] nums, int k) {
+            int len = nums.length;
+            int[] dp = new int[len];
+            dp[len - 1] = nums[len - 1];
+            int maxIndex = len - 1;
+            Deque<Integer> deque = new LinkedList<>();  // storing index
+            deque.addLast(len - 1);
+
+            for (int i = len - 2; i >= 0; i--){
+                while( !deque.isEmpty() && dp[deque.peekFirst()] <= dp[i + 1]){
+                    deque.pollFirst();
+                }
+                deque.addFirst(i + 1);
+
+                if(deque.peekLast() > i + k){
+                    deque.pollLast();
+                }
+
+                dp[i] = nums[i] + dp[deque.peekLast()];
+            }
+            return dp[0];
+        }
+    }
 }
