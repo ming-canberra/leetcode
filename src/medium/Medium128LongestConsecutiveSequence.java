8e0caaf7849bfe2acfccbd67a39c2154da6256c6
Ming <ming.edentiti@gmail.com>
2021-10-20 19:10:08 +1100

    2 bugs, overtime, do this again.

---
+++ src/medium/Medium128LongestConsecutiveSequence.java
@@ -0,0 +1,38 @@
+package medium;
+
+import java.util.*;
+public class Medium128LongestConsecutiveSequence {
+    class Solution {
+        public int longestConsecutive(int[] nums) {
+            int result = 0;
+
+            Map<Integer, int[]> map = new HashMap<>();
+
+            for (int iValue : nums){
+                if (!map.containsKey(iValue)){
+
+                    int max = iValue;
+                    int min = iValue;
+                    if (map.containsKey(iValue + 1)){
+                        max = map.get(map.get(iValue + 1)[1])[1];
+                    }
+                    if (map.containsKey(iValue - 1)){
+                        min = map.get(map.get(iValue - 1)[0])[0];
+                    }
+
+                    if (map.containsKey(iValue + 1)){
+                        map.put(map.get(iValue + 1)[1], new int[]{min, max});
+                    }
+
+                    if (map.containsKey(iValue - 1)){
+                        map.put(map.get(iValue - 1)[0], new int[]{min, max});
+                    }
+
+                    map.put(iValue, new int[]{min, max});
+                    result = Math.max(result, max - min + 1);
+                }
+            }
+            return result;
+        }
+    }
+}
