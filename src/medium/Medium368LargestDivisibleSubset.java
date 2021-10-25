890b4f23cb4600d12dba47ceaaaf08f09299e743
Ming <ming.edentiti@gmail.com>
2021-10-25 21:11:39 +1100

    nailed it within 46 mins

--- src/medium/Medium368LargestDivisibleSubset.java
+++ src/medium/Medium368LargestDivisibleSubset.java
@@ -35,4 +35,37 @@ public class Medium368LargestDivisibleSubset {
             return  dp.get(resultIndex);
         }
     }
+
+    class Solution1 {
+        public List<Integer> largestDivisibleSubset(int[] nums) {
+            Arrays.sort(nums);
+            int len = nums.length;
+            int[][] dp = new int[len][2]; // 0th for length, 1th for previous index;
+            int indexLargest = 0;
+            for (int i = 0; i < len; i++){
+                int curLargest = 1;
+                int indexPrevLargest = 0;
+                for (int j = 0; j < i; j++){
+                    if (nums[i] % nums[j] == 0){
+                        if (curLargest < dp[j][0] + 1){
+                            curLargest = dp[j][0] + 1;
+                            indexPrevLargest = j;
+                        }
+                    }
+                }
+                dp[i][0] = curLargest;
+                dp[i][1] = indexPrevLargest;
+                if (curLargest > dp[indexLargest][0]){
+                    indexLargest = i;
+                }
+            }
+            List<Integer> result = new ArrayList<>();
+            while(dp[indexLargest][0] != 1){
+                result.add(nums[indexLargest]);
+                indexLargest = dp[indexLargest][1];
+            }
+            result.add(nums[indexLargest]);
+            return result;
+        }
+    }
 }
