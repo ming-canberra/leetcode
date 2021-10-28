095b59786dbefa0599a08999e75800c836bf4341
Ming <ming.edentiti@gmail.com>
2021-10-28 19:09:45 +1100

    nailed it within 25 mins

---
+++ src/medium/Medium0216CombinationSumIII.java
@@ -0,0 +1,33 @@
+package medium;
+
+import java.util.*;
+
+public class Medium0216CombinationSumIII {
+    class Solution {
+        List<List<Integer>> result = new ArrayList<>();
+        public List<List<Integer>> combinationSum3(int k, int n) {
+            dfs(n, 1, new ArrayList<>(), 0, k);
+            return result;
+        }
+        private void dfs(int target, int curNum, List<Integer> list, int sum, int k){
+            if (list.size() == k){
+                if (target == sum){
+                    result.add(new ArrayList<>(list));
+                }
+                return;
+            }
+            if (target <= sum){
+                return;
+            }
+
+            if (curNum + sum > target){
+                return;
+            }
+            for (int i = curNum; i < 10; i++){
+                list.add(i);
+                dfs(target, i + 1, list, sum + i, k);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
