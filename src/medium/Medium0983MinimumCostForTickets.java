1b7547627014681321f603a2bd57b5a1c2957de6
Ming <ming.edentiti@gmail.com>
2021-10-28 13:58:17 +1100

    took ages, do this again

--- src/medium/Medium0983MinimumCostForTickets.java
+++ src/medium/Medium0983MinimumCostForTickets.java
@@ -32,4 +32,45 @@ public class Medium0983MinimumCostForTickets {
             return dp[dp.length - 1];
         }
     }
+
+    class Solution1 {
+        public int mincostTickets(int[] days, int[] costs) {
+            Set<Integer> set = new HashSet<>();
+            int lastDay = 0;
+            for (int i : days){
+                set.add(i);
+                lastDay = Math.max(lastDay, i);
+            }
+            int[] dp = new int[lastDay + 1];
+            dp[0] = 0;
+            int cost1 = costs[0];
+            int cost7 = costs[1];
+            int cost30 = costs[2];
+            for (int i = 1; i < dp.length; i++){
+                int min = Integer.MAX_VALUE;
+                if (set.contains(i)){
+                    if (i - 1 >= 0){
+                        min = Math.min(min, dp[i - 1] + Math.min(cost1, Math.min(cost7, cost30)) );
+                    }
+                    if (i - 7 >= 0){
+                        min = Math.min(min, dp[i - 7] + Math.min(cost7, cost30));
+                    }
+                    else{
+                        min = Math.min(min, Math.min(cost7, cost30));
+                    }
+                    if (i - 30 >= 0){
+                        min = Math.min(min, dp[i - 30] + cost30);
+                    }
+                    else{
+                        min = Math.min(min, cost30);
+                    }
+                }
+                else{
+                    min = dp[i - 1];
+                }
+                dp[i] = min;
+            }
+            return dp[lastDay];
+        }
+    }
 }
