c341caea166d5195d7ee28fabd1e04837837549a
Ming <ming.edentiti@gmail.com>
2021-10-29 13:26:27 +1100

    seriously overtime, do this again.

--- src/medium/Medium1105FillingBookcaseShelves.java
+++ src/medium/Medium1105FillingBookcaseShelves.java
@@ -32,4 +32,33 @@ public class Medium1105FillingBookcaseShelves {
             return dp[len - 1];
         }
     }
+    class Solution1 {
+        public int minHeightShelves(int[][] books, int shelfWidth) {
+            int dp[] = new int[books.length];
+            // book [0] thickness    book [1]  height
+            dp[0] = books[0][1];
+            for (int i = 1; i < books.length; i++){
+                int minHeight = Integer.MAX_VALUE;
+                int widthSum = 0;
+                int curHeight = Integer.MIN_VALUE;
+                for (int j = i; j >= 0; j--){
+                    curHeight = Math.max(curHeight, books[j][1]);
+                    widthSum += books[j][0];
+                    if (shelfWidth >= widthSum){
+                        if (j == 0){
+                            minHeight = Math.min(minHeight, curHeight);
+                        }
+                        else{
+                            minHeight = Math.min(minHeight, dp[j - 1] + curHeight);
+                        }
+                    }
+                    else{
+                        break;
+                    }
+                }
+                dp[i]= minHeight;
+            }
+            return dp[books.length - 1];
+        }
+    }
 }
