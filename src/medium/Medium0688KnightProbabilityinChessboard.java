832b110d57dca1db1bc7de9ec4fe72cc68b26538
Ming <ming.edentiti@gmail.com>
2021-10-31 01:43:25 +1100

    did it within 40 mins, 1 bug though

--- src/medium/Medium0688KnightProbabilityinChessboard.java
+++ src/medium/Medium0688KnightProbabilityinChessboard.java
@@ -39,4 +39,33 @@ public class Medium0688KnightProbabilityinChessboard {
             return sum;
         }
     }
+
+    class Solution1 {
+        int[][] dirs = new int[][]{new int[]{2, 1}, new int[]{2, -1}, new int[]{-2, 1}, new int[]{-2, -1},
+                new int[]{1, 2}, new int[]{1, -2}, new int[]{-1, 2}, new int[]{-1, -2}};
+        double[][][] memo;  // the probability of out of boundary
+        public double knightProbability(int n, int k, int row, int column) {
+            memo = new double[n][n][k + 1];
+            return 1.0 - backtrack(n, k, row, column, 1.0);
+        }
+        private double backtrack(int n, int k, int row, int col, double curP){
+            if (row < 0 || col < 0 || row >= n || col >= n){
+                return curP;
+            }
+            if (k == 0){
+                return 0.0;
+            }
+            if (memo[row][col][k] != 0.0){
+                return memo[row][col][k];
+            }
+            double sum = 0.0;
+            for (int i = 0; i < dirs.length; i++){
+                int x = row + dirs[i][0];
+                int y = col + dirs[i][1];
+                sum += backtrack(n, k - 1, x, y, curP * 0.125);
+            }
+            memo[row][col][k] = sum;
+            return memo[row][col][k];
+        }
+    }
 }
