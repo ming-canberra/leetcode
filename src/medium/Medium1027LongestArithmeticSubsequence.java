ca920d263b3c9913a5fcb6639bb54a516531d987
Ming <ming.edentiti@gmail.com>
2021-10-31 18:55:49 +1100

    nailed it within 21 mins

--- src/medium/Medium1027LongestArithmeticSubsequence.java
+++ src/medium/Medium1027LongestArithmeticSubsequence.java
@@ -30,4 +30,26 @@ public class Medium1027LongestArithmeticSubsequence {
             return result;
         }
     }
+
+    class Solution1 {
+        public int longestArithSeqLength(int[] nums) {
+            List<Map<Integer, Integer>> mapList = new ArrayList<>();
+            int result = 2;
+            for (int i = 0; i < nums.length; i++){
+                Map<Integer, Integer> map = new HashMap<>();// diff as key, count as value
+                mapList.add(map);
+                for (int j = 0; j < i; j++){
+                    int diff = nums[i] - nums[j];
+                    int diffCount = mapList.get(i).getOrDefault(diff, 2);
+
+                    if ( mapList.get(j).containsKey(diff) ){
+                        diffCount = Math.max(diffCount, mapList.get(j).get(diff) + 1);
+                    }
+                    result = Math.max(result, diffCount);
+                    mapList.get(i).put(diff, diffCount);
+                }
+            }
+            return result;
+        }
+    }
 }
