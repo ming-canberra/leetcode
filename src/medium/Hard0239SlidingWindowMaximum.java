7e7618c802aea9708d2895027548a261b5a67bf1
Ming <ming.edentiti@gmail.com>
2021-10-31 17:03:31 +1100

    bug free, within 38 mins

--- src/medium/Hard0239SlidingWindowMaximum.java
+++ src/medium/Hard0239SlidingWindowMaximum.java
@@ -24,4 +24,28 @@ public class Hard0239SlidingWindowMaximum {
             return result;
         }
     }
+
+    class Solution1 {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            List<Integer> result = new ArrayList<>();
+            Deque<Integer> deque = new LinkedList<>();// index, in ascending order
+            for (int i = 0; i < nums.length; i++){
+                while(!deque.isEmpty() && nums[deque.peekFirst()] <= nums[i] ){
+                    deque.pollFirst();
+                }
+                deque.addFirst(i);
+                if (i - deque.peekLast() >= k ){
+                    deque.pollLast();
+                }
+                if (i >= k - 1){
+                    result.add(nums[deque.peekLast()]);
+                }
+            }
+            int[] array = new int[result.size()];
+            for (int i = 0; i < result.size(); i++){
+                array[i] = result.get(i);
+            }
+            return array;
+        }
+    }
 }
