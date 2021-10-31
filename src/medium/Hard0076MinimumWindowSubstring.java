d58dc4b144a4c9533efe7837c93d3dfc25f58f18
Ming <ming.edentiti@gmail.com>
2021-10-31 23:36:33 +1100

    I came up with the algo. some bugs, overtime, do this again

--- src/medium/Hard0076MinimumWindowSubstring.java
+++ src/medium/Hard0076MinimumWindowSubstring.java
@@ -1,5 +1,7 @@
 package medium;
 
+import java.util.*;
+
 public class Hard0076MinimumWindowSubstring {
     class Solution {
         public String minWindow(String s, String t) {
@@ -33,6 +35,51 @@ public class Hard0076MinimumWindowSubstring {
                 }
             }
             return resultRight == 0 ? "" : s.substring(resultLeft, resultRight);
+        }
+    }
+
+    class Solution1 {
+        public String minWindow(String s, String t) {
+            int m = s.length();
+            int n = t.length();
+            if (m < n){
+                return "";
+            }
+            Map<Character, Integer> tMap = new HashMap<>();  // char as key, count as value
+            for (char c : t.toCharArray()){
+                int cCount = tMap.getOrDefault(c, 0) + 1;
+                tMap.put(c, cCount);
+            }
+            int charCount = tMap.size();
+            String result = "";
+            int left = 0;
+            for (int right = 0; right < s.length(); right++){
+                char curChar = s.charAt(right);
+                if (tMap.containsKey(curChar)){
+                    if (tMap.get(curChar) == 1){
+                        charCount--;
+                        if (charCount == 0){
+                            // init result
+                            result = s.substring(left, right + 1);
+                        }
+                    }
+                    tMap.put(curChar, tMap.get(curChar) - 1);
+                    if (charCount == 0){
+                        // move left until current left count = 0
+                        while(!tMap.containsKey(s.charAt(left)) || tMap.get( s.charAt(left) ) != 0){
+                            if ( tMap.containsKey(s.charAt(left)) ){
+                                tMap.put(s.charAt(left),  tMap.get(s.charAt(left)) + 1 );
+                            }
+                            left++;
+                        }
+                        String newSub = s.substring(left, right + 1);
+                        if (newSub.length() < result.length()){
+                            result = newSub;
+                        }
+                    }
+                }
+            }
+            return result;
         }
     }
 }
