a4b1e1bea3a548244d5fafd259d8f4bfdf6174a5
Ming <ming.edentiti@gmail.com>
2021-11-02 21:23:05 +1100

    referred to solution, do this again

--- src/medium/Hard0895MaximumFrequencyStack.java
+++ src/medium/Hard0895MaximumFrequencyStack.java
@@ -30,4 +30,29 @@ public class Hard0895MaximumFrequencyStack {
             return value;
         }
     }
+
+    class FreqStack1 {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        Map<Integer, Stack<Integer>> freqStacks = new HashMap<>();
+        int maxFreq = 0;
+        public FreqStack1() {
+        }
+        public void push(int val) {
+            int freq = freqMap.getOrDefault(val, 0) + 1;
+            freqMap.put(val, freq);
+            maxFreq = Math.max(maxFreq, freq);
+            Stack<Integer> stack = freqStacks.getOrDefault(freq, new Stack<>());
+            stack.add(val);
+            freqStacks.put(freq, stack);
+        }
+        public int pop() {
+            Stack<Integer> maxFreqStack = freqStacks.get(maxFreq);
+            int top = maxFreqStack.pop();
+            if (maxFreqStack.isEmpty()){
+                maxFreq--;
+            }
+            freqMap.put(top, freqMap.get(top) - 1);
+            return top;
+        }
+    }
 }
