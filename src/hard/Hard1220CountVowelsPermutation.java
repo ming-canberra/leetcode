80531d1342517dae3818c25176f54dbac048a81d
Ming <ming.edentiti@gmail.com>
2021-11-24 01:18:40 +1100

    nailed it within 30 mins

--- src/hard/Hard1220CountVowelsPermutation.java
+++ src/hard/Hard1220CountVowelsPermutation.java
@@ -33,5 +33,20 @@ public class Hard1220CountVowelsPermutation {
         }
     }
 
-
+    class Solution1 {
+        public int countVowelPermutation(int n) {
+            long mod = 1000000007;
+            long[][] dp = new long[n][5];
+            dp[0] = new long[]{1L, 1L, 1L, 1L, 1L};
+            for (int i = 1; i < n; i++){
+                dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % mod;
+                dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
+                dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % mod;
+                dp[i][3] = dp[i - 1][2];
+                dp[i][4] = (dp[i - 1][2] + dp[i - 1][3]) % mod;
+            }
+            long result = (dp[n - 1][0] + dp[n - 1][1] + dp[n - 1][2] + dp[n - 1][3] + dp[n - 1][4]) % mod;
+            return (int) result;
+        }
+    }
 }
