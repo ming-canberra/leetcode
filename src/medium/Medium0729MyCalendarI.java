55fa4d02779edf83e43c5bb1c9770e162ef16607
Ming <ming.edentiti@gmail.com>
2021-11-26 00:10:07 +1100

    nailed it within 35 mins

---
+++ src/medium/Medium0729MyCalendarI.java
@@ -0,0 +1,73 @@
+package medium;
+
+import java.util.*;
+
+public class Medium0729MyCalendarI {
+    class MyCalendar {
+        List<int[]> list = new ArrayList<>();
+        public MyCalendar() {
+        }
+        public boolean book(int start, int end) {
+            if (list.isEmpty()){
+                list.add(new int[]{start, end});
+                return true;
+            }
+            else{
+                int left = -1;
+                int right = list.size();
+                // find the index where equals to start, or biggest element less than start
+                while(left + 1 < right){
+                    int m = (left + right) / 2;
+                    if (list.get(m)[0] == start){
+                        return false;
+                    }
+                    else if (list.get(m)[0] < start){
+                        left = m;
+                    }
+                    else{
+                        right = m;
+                    }
+                }
+                // all elements are greater than start, check if this can be at 0th
+                if (left == -1){
+                    if (list.get(0)[0] >= end ){
+                        list.add(0, new int[]{start, end});
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    // check if this can sit at left + 1 idx
+                    if (left + 1 == list.size()){
+                        //check left element only
+                        if (list.get(left)[1] <= start ){
+                            list.add(left + 1, new int[]{start, end});
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        // check both left element and left + 1 element
+                        if (list.get(left)[1] <= start && end <= list.get(left + 1)[0] ){
+                            list.add(left + 1, new int[]{start, end});
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
+}
