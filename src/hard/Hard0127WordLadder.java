854d15b85a4d7449b16f683faf6f73c2faf05db7
Ming <ming.edentiti@gmail.com>
2022-01-08 11:23:15 +1100

    within 30 mins, 1 bug, not optimal

--- src/hard/Hard0127WordLadder.java
+++ src/hard/Hard0127WordLadder.java
@@ -78,4 +78,67 @@ public class Hard0127WordLadder {
         }
         return index == a.length();
     }
+
+    class Solution {
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            if (!wordList.contains(endWord)){
+                return 0;
+            }
+            Queue<Integer> queue = new LinkedList<>();
+            int result = 0;
+            int len = wordList.size();
+            boolean[][] connections = new boolean[len][len];
+            for (int i = 0; i < len - 1; i++){
+                for (int j = i + 1; j < len; j++){
+                    if (connected(wordList.get(i), wordList.get(j))){
+                        connections[i][j] = true;
+                        connections[j][i] = true;
+                    }
+                }
+            }
+            if (wordList.contains(beginWord)){
+                queue.add(wordList.indexOf(beginWord));
+                result++;
+            }
+            else{
+                for (int i = 0; i < wordList.size(); i++){
+                    String word = wordList.get(i);
+                    if (connected(word, beginWord)){
+                        queue.add(i);
+                        result += 2;
+                    }
+                }
+            }
+            Set<Integer> visited = new HashSet<>();
+            while(!queue.isEmpty()){
+                for (int size = queue.size(); size > 0; size--){
+                    int top = queue.poll();
+                    if (!visited.contains(top)){
+                        visited.add(top);
+                        if(endWord.equals(wordList.get(top))){
+                            return result;
+                        }
+                        else{
+                            for (int i = 0; i < len; i++){
+                                if (connections[top][i]){
+                                    queue.add(i);
+                                }
+                            }
+                        }
+                    }
+                }
+                result++;
+            }
+            return 0;
+        }
+        private boolean connected(String a, String b){
+            int counter = 0;
+            for (int i = 0; i < a.length(); i++){
+                if (a.charAt(i) != b.charAt(i)){
+                    counter++;
+                }
+            }
+            return counter == 1;
+        }
+    }
 }
