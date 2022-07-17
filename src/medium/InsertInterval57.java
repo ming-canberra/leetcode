9e35818803332df0222a3b93d66ced4450e56444
Ming Chen <ming.canberra@gmail.com>
2022-07-17 20:13:29 +1000

    this one is hard. so many corner cases. Do this again and again. Is there a smart way?

--- src/medium/InsertInterval57.java
+++ src/medium/InsertInterval57.java
@@ -46,4 +46,59 @@ public class InsertInterval57 {
             return result;
         }
     }
+
+
+    class Solution1 {
+        public int[][] insert(int[][] intervals, int[] newInterval) {
+            boolean added = false;
+            List<int[]> res = new ArrayList<>();
+            if (intervals.length == 0) {
+                res.add(newInterval);
+            }
+            else{
+                Integer curStart = null;
+                Integer curEnd = null;
+                for (int i = 0; i < intervals.length; i++) {
+                    if (curStart == null) {
+                        if (intervals[i][0] > newInterval[1]) {
+                            if (!added) {
+                                res.add(newInterval);
+                                added = true;
+                            }
+                            res.add(intervals[i]);
+                        }
+                        else if (intervals[i][1] < newInterval[0]) {
+                            res.add(intervals[i]);
+                            if (i == intervals.length - 1) {
+                                res.add(newInterval);
+                            }
+                        }
+                        else {
+                            curStart = Math.min(intervals[i][0], newInterval[0]);
+                            curEnd = Math.max(intervals[i][1], newInterval[1]);
+                        }
+                    }
+                    else{
+                        if (curEnd < intervals[i][0]) {
+                            added = true;
+                            res.add(new int[]{curStart, curEnd});
+                            res.add(intervals[i]);
+                            curStart = null;
+                            curEnd = null;
+                        }
+                        else{
+                            curEnd = Math.max(intervals[i][1], curEnd);
+                        }
+                    }
+                }
+                if (curStart != null) {
+                    res.add(new int[]{curStart, curEnd});
+                }
+            }
+
+            int[][] intArray = new int[res.size()][];
+            for(int i = 0; i < res.size(); i++) intArray[i] = res.get(i);
+            return intArray;
+        }
+    }
 }
