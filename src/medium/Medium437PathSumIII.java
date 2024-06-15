f8922d100e045bfb5acf439f67001d650678f99a
Ming Chen <ming.canberra@gmail.com>
2024-06-16 00:49:05 +1000

    this one did not pass all test cases, but I think it is ok.

--- src/medium/Medium437PathSumIII.java
+++ src/medium/Medium437PathSumIII.java
@@ -67,4 +67,49 @@ public class Medium437PathSumIII {
             map.put(sum, map.get(sum) - 1);
         }
     }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution1 {
+        public int pathSum(TreeNode root, int targetSum) {
+            if (root == null) {
+                return 0;
+            }
+            int countIncludingRoot = 0;
+            if (root.val == targetSum) {
+                countIncludingRoot++;
+            }
+            int leftCount = sumIncludingRoot(root.left, targetSum - root.val);
+            int rightCount = sumIncludingRoot(root.right, targetSum - root.val);
+            countIncludingRoot = countIncludingRoot + leftCount + rightCount;
+            return countIncludingRoot + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+        }
+
+        private int sumIncludingRoot(TreeNode root, int targetSum) {
+            if (root == null) {
+                return 0;
+            }
+            int count = 0;
+
+            if (root.val == targetSum) {
+                count++;
+            }
+            int leftCount = sumIncludingRoot(root.left, targetSum - root.val);
+            int rightCount = sumIncludingRoot(root.right, targetSum - root.val);
+            return count + leftCount + rightCount;
+        }
+    }
 }
